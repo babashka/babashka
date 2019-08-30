6fed8c33279a6f059cd1f52ba5b2eddd4cb6b904
Michiel Borkent <michielborkent@gmail.com>
2019-08-30 22:59:13 +0200

    kinda works

--- src/babashka/impl/socket_repl.clj
+++ src/babashka/impl/socket_repl.clj
@@ -1,7 +1,11 @@
 (ns babashka.impl.socket-repl
   (:require [babashka.impl.clojure.server :as server]
             [babashka.impl.clojure.main :as m]
-            [sci.core :refer [eval-string]])
+            [sci.core :refer [eval-string]]
+            [sci.impl.parser :as parser]
+            [sci.impl.toolsreader.v1v3v2.clojure.tools.reader.reader-types :as r]
+            [sci.impl.toolsreader.v1v3v2.clojure.tools.reader.edn :as edn]
+            [sci.core :as sci])
   (:import [babashka.impl LockFix]))
 
 (def env (atom {}))
@@ -14,7 +18,15 @@
                                            :bindings {'*1 *1
                                                       '*2 *2
                                                       '*3 *3
-                                                      '*e *e}}))))
+                                                      '*e *e}}))
+          :read (fn [request-prompt request-exit]
+                  (let [in (r/indexing-push-back-reader (r/push-back-reader *in*))]
+                    (parser/parse-next {} in)))))
+
+#_(defn accept []
+  (prn "ACCEPT!")
+  (def i *in*)
+  @(promise))
 
 (defn start-repl! [port]
   (prn "REPL!" port)
@@ -23,5 +35,8 @@
     :name "bb"
     :accept babashka.impl.socket-repl/repl}))
 
-
+(comment
+  (start-repl! 1666)
+  (server/stop-server "bb")
+ )
 
