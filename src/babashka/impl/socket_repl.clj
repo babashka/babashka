851fb4b5459459fef5c5dfb9816976a5ec0884ea
Michiel Borkent <michielborkent@gmail.com>
2019-09-01 09:20:09 +0200

    [#44] fix ctrl-D in socket-repl

--- src/babashka/impl/socket_repl.clj
+++ src/babashka/impl/socket_repl.clj
@@ -1,43 +1,45 @@
 (ns babashka.impl.socket-repl
   {:no-doc true}
-  (:require [babashka.impl.clojure.core.server :as server]
-            [babashka.impl.clojure.main :as m]
-            [sci.core :refer [eval-string]]
-            [sci.impl.parser :as parser]
-            [sci.impl.toolsreader.v1v3v2.clojure.tools.reader.reader-types :as r]
-            [clojure.string :as str]
-            [clojure.java.io :as io]))
+  (:require
+   [babashka.impl.clojure.core.server :as server]
+   [babashka.impl.clojure.main :as m]
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [sci.core :refer [eval-string]]
+   [sci.impl.parser :as parser]
+   [sci.impl.toolsreader.v1v3v2.clojure.tools.reader.reader-types :as r]))
 
 (set! *warn-on-reflection* true)
 
 (defn repl
   "REPL with predefined hooks for attachable socket server."
   [sci-opts]
-  (m/repl
-   :init #(do (println "Babashka"
-                       (str "v" (str/trim (slurp (io/resource "BABASHKA_VERSION"))))
-                       "REPL.")
-              (println "Use :repl/quit or :repl/exit to quit the REPL.")
-              (println "Clojure rocks, Bash reaches.")
-              (println))
-   :read (fn [request-prompt request-exit]
-           (let [in (r/indexing-push-back-reader (r/push-back-reader *in*))
-                 p (r/peek-char in)]
-             (if (= \newline p)
-               (do (r/read-char in) request-prompt)
-               (let [v (parser/parse-next {} in)]
-                 (if (or (identical? :repl/quit v)
-                         (identical? :repl/exit v))
-                   request-exit
-                   v)))))
-   :eval (fn [expr]
-           (eval-string (str expr)
-                        (update sci-opts
-                                :bindings
-                                merge {'*1 *1
-                                       '*2 *2
-                                       '*3 *3
-                                       '*e *e})))))
+  (let [in (r/indexing-push-back-reader (r/push-back-reader *in*))]
+    (m/repl
+     :init #(do (println "Babashka"
+                         (str "v" (str/trim (slurp (io/resource "BABASHKA_VERSION"))))
+                         "REPL.")
+                (println "Use :repl/quit or :repl/exit to quit the REPL.")
+                (println "Clojure rocks, Bash reaches.")
+                (println))
+     :read (fn [request-prompt request-exit]
+             (if-let [p (r/peek-char in)]
+               (if (= \newline p)
+                 (do (r/read-char in) request-prompt)
+                 (let [v (parser/parse-next {} in)]
+                   (if (or (identical? :repl/quit v)
+                           (identical? :repl/exit v))
+                     request-exit
+                     v)))
+               request-exit))
+     :eval (fn [expr]
+             (eval-string (str expr)
+                          (update sci-opts
+                                  :bindings
+                                  merge {'*1 *1
+                                         '*2 *2
+                                         '*3 *3
+                                         '*e *e}))))))
 
 (defn start-repl! [host+port sci-opts]
   (let [parts (str/split host+port #":")
