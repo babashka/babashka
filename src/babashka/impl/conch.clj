a7c95d780b1a22512738b1f81c4f10bd7bcd327c
Michiel Borkent <michielborkent@gmail.com>
2019-09-02 23:57:39 +0200

    wip

--- src/babashka/impl/conch.clj
+++ src/babashka/impl/conch.clj
@@ -1,8 +1,19 @@
 (ns babashka.impl.conch
-  (:require [me.raynes.conch.low-level :as ll]))
+  (:require
+   [me.raynes.conch.low-level :as ll]
+   [me.raynes.conch :as conch]))
+
+(defn programs
+  "Creates functions corresponding to progams on the PATH, named by names."
+  {:sci/macro true}
+  [& names]
+  `(do ~@(for [name names]
+           `(defn ~name [& ~'args]
+              (apply ~'conch/execute ~(str name) ~'args)))))
 
 (def conch-bindings
-  {'conch/proc ll/proc
+  {;; low level API
+   'conch/proc ll/proc
    'conch/destroy ll/destroy
    'conch/exit-code ll/exit-code
    'conch/flush ll/flush
@@ -12,4 +23,7 @@
    'conch/stream-to-string ll/stream-to-string
    'conch/stream-to-out ll/stream-to-out
    'conch/feed-from-string ll/feed-from-string
-   'conch/read-line ll/read-line})
+   'conch/read-line ll/read-line
+   ;; high level API
+   'conch/execute conch/execute
+   'conch/programs (with-meta programs (meta #'programs))})
