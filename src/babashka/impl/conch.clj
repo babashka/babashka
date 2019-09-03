f8676aea8455b3c14250c1fd508e3ee82ae5a633
Michiel Borkent <michielborkent@gmail.com>
2019-09-03 13:41:36 +0200

    wip

--- src/babashka/impl/conch.clj
+++ src/babashka/impl/conch.clj
@@ -5,11 +5,35 @@
 
 (defn programs
   "Creates functions corresponding to progams on the PATH, named by names."
-  {:sci/macro true}
   [& names]
   `(do ~@(for [name names]
            `(defn ~name [& ~'args]
               (apply ~'conch/execute ~(str name) ~'args)))))
+
+(defn defcommands
+  "Creates functions corresponding to progams on the PATH, named by names."
+  [& names]
+  `(do ~@(for [name names]
+           `(defn ~name [& ~'args]
+              (apply ~'conch/proc ~(str name) ~'args)))))
+
+(defn pipe [p1 p2]
+  ;; TODO varargs
+  (ll/stream-to p1 :out (:in p2))
+  p2)
+
+(defn out-str [p]
+  (ll/done p)
+  (ll/stream-to-string p :out))
+
+(defn in-str [p s]
+  (ll/feed-from-string p s))
+
+(defn eof [p]
+  (ll/done p))
+
+(defn err-str [p]
+  (ll/stream-to-string p :err))
 
 (def conch-bindings
   {;; low level API
@@ -26,4 +50,11 @@
    'conch/read-line ll/read-line
    ;; high level API
    'conch/execute conch/execute
-   'conch/programs (with-meta programs (meta #'programs))})
+   'conch/programs (with-meta programs {:sci/macro true})
+
+   'proc/proc ll/proc
+   'proc/defcommands (with-meta defcommands {:sci/macro true})
+   'proc/pipe pipe
+   'proc/out-str out-str
+   'proc/in-str in-str
+   'proc/eof eof})
