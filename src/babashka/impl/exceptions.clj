e6674ef31ee839b74253f2b8e62d83b417b09b46
Michiel Borkent <michielborkent@gmail.com>
2019-09-26 00:24:03 +0300

    upgrade sci for try/catch, add exception bindings

---
+++ src/babashka/impl/exceptions.clj
@@ -0,0 +1,7 @@
+(ns babashka.impl.exceptions)
+
+(def exception-bindings
+  {'Exception Exception
+   'java.lang.Exception Exception
+   'ArithmeticException ArithmeticException
+   'java.lang.ArithmeticException ArithmeticException})
