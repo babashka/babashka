9729c6ff5d65834e6516d53e9562dc2dbc0104e4
Michiel Borkent <michielborkent@gmail.com>
2019-10-29 12:49:18 +0100

    [#89] use :namespaces option for clojure.core rather than :bindings

--- src/babashka/impl/clojure/core.clj
+++ src/babashka/impl/clojure/core.clj
@@ -6,39 +6,26 @@
   [_ _ & body]
   `(~'future-call (fn [] ~@body)))
 
-(def core-bindings
-  {'atom atom
-   'swap! swap!
-   'swap-vals! swap-vals!
-   'reset! reset!
-   'add-watch add-watch
-   'future-call future-call
+(def core-extras
+  {'future-call future-call
    'future (with-meta future {:sci/macro true})
    'future-cancel future-cancel
    'future-cancelled? future-cancelled?
    'future-done? future-done?
    'future? future?
-   'deref deref
    'agent agent
    'send send
    'send-off send-off
    'promise promise
    'deliver deliver
    'shutdown-agents shutdown-agents
-   'run! run!
    'slurp slurp
    'spit spit
    'pmap pmap
    'pr pr
-   'pr-str pr-str
    'prn prn
-   'prn-str prn-str
-   'print-str print-str
    'print print
    'println println
    'println-str println-str
    'flush flush
-   'ex-info ex-info
-   'ex-data ex-data
-   'read-line read-line
-   'namespace namespace})
+   'read-line read-line})
