a01f0c65685dd11a34d74512295c69a5409251a1
Michiel Borkent <michielborkent@gmail.com>
2019-12-12 22:50:26 +0100

    tests

--- test/babashka/classpath_test.clj
+++ test/babashka/classpath_test.clj
@@ -13,9 +13,14 @@
              "(require '[my-script :as ms]) (ms/foo)")))
   (is (= "hello from foo\n"
          (tu/bb nil "--classpath" "test-resources/babashka/src_for_classpath_test/foo.jar"
-                "(require '[foo :as f]) (f/foo)")))
+                "(require '[foo :as f]) (f/foo)"))))
+
+(deftest classpath-env-test
   ;; for this test you have to set `BABASHKA_CLASSPATH` to test-resources/babashka/src_for_classpath_test/env
-  (is (= "env!" (bb nil "(require '[env-ns]) (env-ns/foo)"))))
+  ;; and `BABASHKA_PRELOADS` to "(require '[env-ns])"
+  (when (System/getenv "BABASHKA_CLASSPATH_TEST")
+    (println (System/getenv "BABASHKA_CLASSPATH"))
+    (is (= "env!" (bb nil "(env-ns/foo)")))))
 
 (deftest main-test
   (is (= "(\"1\" \"2\" \"3\" \"4\")\n"
