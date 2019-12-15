1974a2362d056cf2d35fe81194ab73a6e90e8fba
Michiel Borkent <michielborkent@gmail.com>
2019-12-15 20:55:16 +0100

    Update sci: fix for nested syntax quote

--- src/babashka/impl/repl.clj
+++ src/babashka/impl/repl.clj
@@ -23,7 +23,7 @@
                 (println))
      :read (fn [_request-prompt request-exit]
              (if (r/peek-char in) ;; if this is nil, we reached EOF
-               (let [v (parser/parse-next in #{:bb} {:current (-> sci-ctx :env deref :current-ns)})]
+               (let [v (parser/parse-next sci-ctx in)]
                  (if (or (identical? :repl/quit v)
                          (identical? :repl/exit v)
                          (identical? :edamame.impl.parser/eof v))
