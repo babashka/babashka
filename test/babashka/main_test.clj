d3c66ee6ce3be905464b20dbe483e55308fddafd
Michiel Borkent <michielborkent@gmail.com>
2019-12-17 11:22:12 +0100

    fix test

--- test/babashka/main_test.clj
+++ test/babashka/main_test.clj
@@ -186,10 +186,10 @@
 
 (deftest wait-for-port-test
   (is (= :timed-out
-         (bb nil "(def web-server (conch/proc \"python\" \"-m\" \"SimpleHTTPServer\" \"7171\"))
-                (wait/wait-for-port \"127.0.0.1\" 7171)
-                (conch/destroy web-server)
-                (wait/wait-for-port \"localhost\" 7172 {:default :timed-out :timeout 50})"))))
+         (bb nil "(def ws (-> (ProcessBuilder. [\"python\" \"-m\" \"SimpleHTTPServer\" \"1777\"]) (.start)))
+                (wait/wait-for-port \"127.0.0.1\" 1777)
+                (.destroy ws)
+                (wait/wait-for-port \"localhost\" 1777 {:default :timed-out :timeout 50})"))))
 
 (deftest wait-for-path-test
   (let [temp-dir-path (System/getProperty "java.io.tmpdir")]
