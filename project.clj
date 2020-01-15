73ec6da56ae4bf493a1cc9791a511a6023623952
Michiel Borkent <michielborkent@gmail.com>
2020-01-15 20:11:44 +0100

    More optimizations

--- project.clj
+++ project.clj
@@ -18,7 +18,8 @@
                  [org.clojure/tools.cli "0.4.2"]
                  [org.clojure/data.csv "0.1.4"]
                  [cheshire "5.9.0"]]
-  :profiles {:test {:dependencies [[clj-commons/conch "0.9.2"]]}
+  :profiles {:test {:dependencies [[clj-commons/conch "0.9.2"]
+                                   [com.clojure-goes-fast/clj-async-profiler "0.4.0"]]}
              :uberjar {:global-vars {*assert* false}
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"
                                   "-Dclojure.spec.skip-macros=true"]
--- test/babashka/profile.clj
+++ test/babashka/profile.clj
@@ -5,8 +5,8 @@
 
 ;; clojure -A:profile -e "(prn (loop [val 0 cnt 1000000] (if (pos? cnt) (recur (inc val) (dec cnt)) val)))"
 
-#_(require '[clj-async-profiler.core :as prof])
+(require '[clj-async-profiler.core :as prof])
 
 (defn -main [& options]
-  #_(prof/profile (apply main/main options))
+  (prof/profile (apply main/main options))
   (shutdown-agents))
