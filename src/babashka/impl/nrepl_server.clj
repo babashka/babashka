7a33147d86454d379208b188219503eb54b2e45e
Michiel Borkent <michielborkent@gmail.com>
2020-03-25 17:54:43 +0100

    delay checking dev setting

--- src/babashka/impl/nrepl_server.clj
+++ src/babashka/impl/nrepl_server.clj
@@ -12,7 +12,7 @@
 (set! *warn-on-reflection* true)
 
 (def port 1667)
-(def dev? (= "true" (System/getenv "BABASHKA_DEV")))
+(def dev? (volatile! nil))
 
 (defn response-for [old-msg msg]
   (let [session (get old-msg :session "none")
@@ -22,7 +22,7 @@
     m))
 
 (defn send [^OutputStream os msg]
-  (when dev? (prn "Sending" msg))
+  (when @dev? (prn "Sending" msg))
   (write-bencode os msg)
   (.flush os))
 
@@ -38,7 +38,7 @@
   (let [ex-map (Throwable->map ex)
         ex-name (-> ex-map :via first :type)
         cause (:cause ex-map)]
-    (when dev? (prn "sending ex" ex-name))
+    (when @dev? (prn "sending ex" ex-name))
     (send os (response-for msg {"err" (str ex-name ": " cause "\n")}))
     #_(send os (response-for msg {"ex" (str "class " )
                                   "err" (with-out-str (stacktrace/print-throwable ex))
@@ -52,7 +52,7 @@
                ::nil
                (sci/binding [sci/out sw] (eval-string* ctx code-str)))
         out-str (not-empty (str sw))]
-    (when dev? (println "out str:" out-str))
+    (when @dev? (println "out str:" out-str))
     (send o (response-for msg (cond-> {"ns" (vars/current-ns-name)}
                                 out-str (assoc "value" out-str))))
     (send o (response-for msg (cond-> {"ns" (vars/current-ns-name)}
@@ -67,15 +67,15 @@
       (update :op keyword)))
 
 (defn session-loop [ctx ^InputStream is os id]
-  (when dev? (println "Reading!" id (.available is)))
+  (when @dev? (println "Reading!" id (.available is)))
   (when-let [msg (try (read-bencode is)
                       (catch EOFException _
                         (println "Client closed connection.")))]
     (let [msg (read-msg msg)]
-      (when dev? (prn "Received" msg))
+      (when @dev? (prn "Received" msg))
       (case (get msg :op)
         :clone (do
-                 (when dev? (println "Cloning!"))
+                 (when @dev? (println "Cloning!"))
                  (let [id (str (java.util.UUID/randomUUID))]
                    (send os (response-for msg {"new-session" id "status" #{"done"}}))
                    (recur ctx is os id)))
@@ -99,18 +99,18 @@
                                                              (vals *clojure-version*))}}}))
             (recur ctx is os id))
         ;; fallback
-        (do (when dev?
+        (do (when @dev?
               (println "Unhandled message" msg))
             (send os (response-for msg {"status" #{"error" "unknown-op" "done"}}))
             (recur ctx is os id))))))
 
 (defn listen [ctx ^ServerSocket listener]
-  (when dev? (println "Listening"))
+  (when @dev? (println "Listening"))
   (let [client-socket (.accept listener)
         in (.getInputStream client-socket)
         in (PushbackInputStream. in)
         out (.getOutputStream client-socket)]
-    (when dev? (println "Connected."))
+    (when @dev? (println "Connected."))
     (sci/future
       (sci/binding
           ;; allow *ns* to be set! inside future
@@ -119,6 +119,7 @@
     (recur ctx listener)))
 
 (defn start-server! [ctx host+port]
+  (vreset! dev? (= "true" (System/getenv "BABASHKA_DEV")))
   (let [parts (str/split host+port #":")
         [address port] (if (= 1 (count parts))
                       [nil (Integer. ^String (first parts))]
