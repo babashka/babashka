54b49869c2c1ed78bb6aae04159b5a9f76c441f1
Michiel Borkent <michielborkent@gmail.com>
2020-04-23 23:00:25 +0200

    Add with-transaction

--- src/babashka/impl/jdbc.clj
+++ src/babashka/impl/jdbc.clj
@@ -6,6 +6,19 @@
 
 (def next-ns (vars/->SciNamespace 'next.jdbc nil))
 
+(defn with-transaction
+  "Given a transactable object, gets a connection and binds it to `sym`,
+  then executes the `body` in that context, committing any changes if the body
+  completes successfully, otherwise rolling back any changes made.
+  The options map supports:
+  * `:isolation` -- `:none`, `:read-committed`, `:read-uncommitted`,
+      `:repeatable-read`, `:serializable`,
+  * `:read-only` -- `true` / `false`,
+  * `:rollback-only` -- `true` / `false`."
+  [_ _ [sym transactable opts] & body]
+  (let [con (vary-meta sym assoc :tag 'java.sql.Connection)]
+    `(next.jdbc/transact ~transactable (^{:once true} fn* [~con] ~@body) ~(or opts {}))))
+
 (def njdbc-namespace
   {'get-datasource (copy-var njdbc/get-datasource next-ns)
    'execute! (copy-var njdbc/execute! next-ns)
@@ -13,4 +26,6 @@
    'get-connection (copy-var njdbc/get-connection next-ns)
    'plan (copy-var njdbc/plan next-ns)
    'prepare (copy-var njdbc/prepare next-ns)
-   'transact (copy-var njdbc/transact next-ns)})
+   'transact (copy-var njdbc/transact next-ns)
+   'with-transaction (with-meta with-transaction
+                       {:sci/macro true})})
