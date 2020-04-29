d29eabe495c938a8a73110d300fa83ba464646dd
Michiel Borkent <michielborkent@gmail.com>
2020-04-29 13:30:48 +0200

    wip

--- src-java/babashka/modules/PgNextJDBC.java
+++ src-java/babashka/modules/PgNextJDBC.java
@@ -1,9 +1,16 @@
 package babashka.modules;
 
 public class PgNextJDBC {
-    private static native String initPG();
+    private static native Object initPG(long isolateThreadId, int x, int y);
+    private static native long createIsolate();
+    //private static native void tearDownIsolate(long isolate);
 
-    public static String init() {
-        return initPG();
+    public static void init(int a, int b) {
+        long isolate = createIsolate();
+        Object x = initPG(isolate, a, b);
+        System.out.println("hello");
+        System.out.println(x);
+        System.out.flush();
+        //tearDownIsolate(isolate);
     }
 }
