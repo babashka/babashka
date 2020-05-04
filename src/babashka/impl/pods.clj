46042e0de804c86f07fadf86006db702ab9583db
Michiel Borkent <michielborkent@gmail.com>
2020-05-04 16:58:36 +0200

    wip

--- src/babashka/impl/pods.clj
+++ src/babashka/impl/pods.clj
@@ -3,6 +3,7 @@
   (:refer-clojure :exclude [read])
   (:require [babashka.impl.bencode.core :as bencode]
             [cheshire.core :as cheshire]
+            [clojure.core.async :as async]
             [clojure.edn :as edn]))
 
 (set! *warn-on-reflection* true)
@@ -21,52 +22,46 @@
 (defn bytes->string [^"[B" bytes]
   (String. bytes))
 
-(def waiters (atom {}))
-
-(defn deliver-value [waiter value done?]
-  (if (instance? clojure.lang.IDeref waiter)
-    (deliver waiter value)
-    :core-async-todo))
-
 (defn processor [pod]
   (let [stdout (:stdout pod)
         format (:format pod)
+        chans (:chans pod)
         read-fn (case format
                   :edn edn/read-string
                   :json #(cheshire/parse-string % true))]
     (loop []
-      (println "reading")
       (try
         (let [reply (read stdout)
-              _ (prn "reply" reply)
               id    (get reply "id")
-              _ (prn "id" id)
               id    (bytes->string id)
               value (get reply "value")
-              value (bytes->string value)
-              value (read-fn value)
-              _ (prn "val" value)
+              value (when value (bytes->string value))
+              value (when value (read-fn value))
               status (get reply "status")
               status (set (map (comp keyword bytes->string) status))
               done? (contains? status :done)
-              waiter (get @waiters id)]
-          (deliver-value waiter value done?))
-        (catch Exception e (prn e))))))
+              chan (get @chans id)]
+          (when value (async/>!! chan value))
+          (when done? (async/close! chan)))
+        (catch Exception e (prn e)))
+      (recur))))
 
-(defn invoke [pod pod-var args]
+(defn invoke [pod pod-var args async?]
   (let [stream (:stdin pod)
         format (:format pod)
+        chans (:chans pod)
         write-fn (case format
                    :edn pr-str
                    :json cheshire/generate-string)
         id (str (java.util.UUID/randomUUID))
-        prom (promise)
-        _ (swap! waiters assoc id prom)
+        chan (async/chan)
+        _ (swap! chans assoc id chan)
         _ (write stream {"id" id
                          "op" "invoke"
                          "var" (str pod-var)
                          "args" (write-fn args)})]
-    @prom))
+    (if async? chan
+        (async/<!! chan))))
 
 (defn load-pod
   ([ctx pod-spec] (load-pod ctx pod-spec nil))
@@ -78,21 +73,24 @@
          stdin (.getOutputStream p)
          stdout (.getInputStream p)
          stdout (java.io.PushbackInputStream. stdout)
-         pod {:process p
-              :pod-spec pod-spec
-              :stdin stdin
-              :stdout stdout}
          _ (add-shutdown-hook! #(.destroy p))
          _ (write stdin {"op" "describe"})
          reply (read stdout)
          format (-> (get reply "format") bytes->string keyword)
-         pod (assoc pod :format format)
+         pod {:process p
+              :pod-spec pod-spec
+              :stdin stdin
+              :stdout stdout
+              :chans (atom {})
+              :format format}
          vars (get reply "vars")
          vars (map (fn [var]
-                     (-> (zipmap (map keyword (keys var))
-                                 (map bytes->string (vals var)))
-                         (update :namespace symbol)
-                         (update :name symbol)))
+                     (let [var (zipmap (map keyword (keys var))
+                                       (map bytes->string (vals var)))
+                           var (-> var
+                                   (update :namespace symbol)
+                                   (update :name symbol))]
+                       var))
                    vars)
          env (:env ctx)]
      (swap! env
@@ -101,16 +99,15 @@
                     namespaces (reduce (fn [acc v]
                                          (let [ns (:namespace v)
                                                name (:name v)
-                                               sym (symbol (str ns) (str name))]
-                                           (prn ns name)
+                                               sym (symbol (str ns) (str name))
+                                               async? (:async v)]
                                            (assoc-in acc [ns name]
                                                      (fn [& args]
                                                        ;; (prn "calling" ns name args)
-                                                       (invoke pod sym args)))))
+                                                       (invoke pod sym args async?)))))
                                        namespaces
                                        vars)]
                 (assoc env :namespaces namespaces))))
-     (println "spinning up worker")
      (future (processor pod))
      vars)))
 
