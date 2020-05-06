a4bea4e6c389ee22c670d0ddc1728d350f3353cc
Michiel Borkent <michielborkent@gmail.com>
2020-05-06 12:00:17 +0200

    pod

--- pods/pod-lispyclouds-sqlite/pod-lispyclouds-sqlite.py
+++ pods/pod-lispyclouds-sqlite/pod-lispyclouds-sqlite.py
@@ -1,19 +1,5 @@
 #!/usr/bin/env python3
 
-# To run this:
-
-# Install Python3 and SQLite3
-# Create a virtualenv: python3 -m venv ~/.virtualenvs/babashka
-# Switch to it: source ~/.virtualenvs/babashka/bin/activate
-# Run: pip install bcoding # to install the bencode lib
-# Create a new db: sqlite3 /tmp/babashka.db "CREATE TABLE foo (foo int);"
-
-# Can be tested standalone as:
-# pods/pod-lispyclouds-sqlite.py <<< $(bb -e '(bencode/write-bencode System/out {"op" "invoke" "var" "pod.lispy-clouds.sqlite/execute!" "id" 1 "args" "[\"insert into foo values(1)\"]"})')
-
-# Run as pod with babashka:
-# bb "(babashka.pods/load-pod [\"pods/pod-lispyclouds-sqlite.py\"]) (require '[pod.lispy-clouds.sqlite :as sqlite]) (sqlite/execute! \"create table if not exists foo ( int foo )\") (sqlite/execute! \"insert into foo values (1), (2)\") (sqlite/execute! \"select * from foo;\")"
-
 import json
 import sqlite3
 import sys
