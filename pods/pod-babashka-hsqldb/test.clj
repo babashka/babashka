424a23d3df94589b3a7a37d3dbc8c7e2d7489945
Michiel Borkent <michielborkent@gmail.com>
2020-05-06 11:51:44 +0200

    wip

---
+++ pods/pod-babashka-hsqldb/test.clj
@@ -0,0 +1,45 @@
+(ns test
+  (:refer-clojure :exclude [read])
+  (:require [bencode.core :as bencode]
+            [clojure.edn :as edn])
+  #_(:import java.lang.ProcessBuilder$Redirect))
+
+(defn write [stream v]
+  (bencode/write-bencode stream v)
+  (.flush stream))
+
+(defn read [stream]
+  (bencode/read-bencode stream))
+
+(defn bytes->string [^"[B" bytes]
+  (String. bytes))
+
+(defn query [stream q]
+  (write stream {"op" "invoke"
+                 "var" "hsqldb.jdbc/execute!"
+                 "args" (pr-str ["jdbc:hsqldb:mem:testdb;sql.syntax_mys=true" q])}))
+
+(let [pb (ProcessBuilder. #_["lein" "run" "-m" "org.babashka.hsqldb"]
+                          ["./hsqldb-babashka-plugin"])
+      _ (.redirectErrorStream pb true)
+      ;; _ (.redirectOutput pb ProcessBuilder$Redirect/INHERIT)
+      p (.start pb)
+      stdin (.getOutputStream p)
+      stdout (.getInputStream p)
+      stdout (java.io.PushbackInputStream. stdout)]
+  (write stdin {"op" "describe"})
+  (let [reply (read stdout)]
+    (println "format:" (String. (get reply "format")))
+    (println "vars:" (mapv bytes->string (get reply "vars")))) ;;=> edn
+
+  (query stdin ["create table foo ( foo int );"])
+  (let [reply (read stdout)]
+    (println "reply:" (edn/read-string (String. (get reply "value"))))) ;;=>  [{:next.jdbc/update-count 0}]
+
+  (query stdin ["insert into foo values ( 1, 2, 3);"])
+  (let [reply (read stdout)]
+    (println "reply:" (edn/read-string (String. (get reply "value"))))) ;;=> [{:next.jdbc/update-count 3}]
+
+  (query stdin ["select * from foo;"])
+  (let [reply (read stdout)]
+    (println "reply:" (edn/read-string (String. (get reply "value")))))) ;=> [{:FOO/FOO 1} {:FOO/FOO 2} {:FOO/FOO 3}]
