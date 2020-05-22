b03a8c959141ab8998cb929c50675de932bbdea5
Sameer Kolhar <kolhar730@gmail.com>
2020-05-22 19:28:07 +0530

    Update bump_graal script with java option

--- script/bump_graal_version.clj
+++ script/bump_graal_version.clj
@@ -14,17 +14,25 @@
 ;; Currently we use GraalVM java8-19.3.2
 
 (ns bump-graal-version
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.tools.cli :as cli]))
 
 (defn display-help []
   (println (->> [""
                  "This is a script that should be run when you'd"
-                 "you'd like to bump the GraalVM version for bb."
+                 "you'd like to bump the GraalVM/java version for bb."
                  ""
-                 "Use it by providing one command line argument"
-                 "i.e the version you'd want to upgrade it to"
+                 "The following options are available:"
+                 "(-g, --graal)  - to specify the GraalVM version"
+                 "(-j, --java)   - to specify the java version"
                  ""
-                 "./bump_graal_version.clj 19.3.2 (the new version)"
+                 "Use it by providing one or 2 command line arguments"
+                 "i.e the version you'd want to upgrade it to and/or the java version"
+                 ""
+                 "./bump_graal_version.clj -g 19.3.2 (the new version)"
+                 "or"
+                 "./bump_graal_version.clj -g 19.3.2 --java java11"
+                 "(for GraalVM java11-19.3.2)"
                  ""]
                 (str/join \newline))))
 
@@ -33,40 +41,64 @@
    ".circleci/config.yml"
    "appveyor.yml"])
 
-;; We might have to keep changing this every time
-;; the version is bumped
-(def current-version "19.3.2")
+;; We might have to keep changing these from
+;; time to time whenever the version is bumped
+;; 
+;; OR 
+;; 
+;; We could have them as environment variables
+(def current-graal-version "19.3.2")
+(def current-java-version "java8")
+(def valid-graal-bumps ["19.3.2" "20.1.0"])
+(def valid-java-bumps ["java8" "java11"])
 
-(def valid-bumps ["19.3.2", "20.1.0"])
+(def cl-options
+  [["-g" "--graal VERSION" "graal version"]
+   ["-j" "--java VERSION" "java version"]
+   ["-h" "--help"]])
+
+(def cl-args
+  (:options (cli/parse-opts *command-line-args* cl-options)))
 
 (defn is-valid-bump?
-  [version]
+  [version valid-bumps]
   (some #(= % version) valid-bumps))
 
-(defn replace-version
-  [file new-version]
+(defn replace-current
+  [file current new]
   (let [file-contents (slurp file)]
-    (str/replace file-contents current-version new-version)))
+    (str/replace file-contents current new)))
 
-(defn bump-version
-  [new-version]
+(defn bump-current
+  [current new]
     (doseq [file files-to-edit]
-      (let [exec-res (replace-version file new-version)]
+      (let [exec-res (replace-current file current new)]
         (try (spit file exec-res)
           (catch Exception e (str "There was an error: " (.getMessage e)))
              (finally
                (println "Done with : " file))))))
 
-(defn exec-script []
-  (let [arg *command-line-args*
-        arg-count (count arg)]
-    (if (not= 1 arg-count)
-      (do
-        (println "Incorrect number of args passed!")
-        (display-help))
-      (let [new-version (first arg)]
-        (if (is-valid-bump? new-version)
-          (bump-version new-version)
-          (println "Please be sure to check if it is a supported version: " new-version))))))
+(defn show-error
+  [err-version]
+  (println "This is not a valid version: " err-version))
 
-(exec-script)
+(defn exec-script
+  [args]
+  (when (empty? args)
+    (display-help))
+  (let [new-graal-version (:graal args)
+        new-java-version (:java args)]
+    (when (not (nil? new-graal-version))
+      (if (is-valid-bump? new-graal-version valid-graal-bumps)
+        (do
+          (println "Performing Graal bump...")
+          (bump-current current-graal-version new-graal-version))
+        (show-error new-graal-version)))
+    (when (not (nil? new-java-version))
+      (if (is-valid-bump? new-java-version valid-java-bumps)
+        (do
+          (println "Performing Java bump...")
+          (bump-current current-java-version new-java-version))
+        (show-error new-java-version)))))
+
+(exec-script cl-args)
