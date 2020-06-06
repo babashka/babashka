ad234422b6b99914fa8f66c61d01a1af6f7331ef
Michiel Borkent <michielborkent@gmail.com>
2020-06-06 11:16:48 +0200

    Graalvm home

--- script/test.bat
+++ script/test.bat
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+if "%GRAALVM_HOME%"=="" (
+echo Please set GRAALVM_HOME
+exit /b
+)
+
 echo "BABASHKA_TEST_ENV: %BABASHKA_TEST_ENV%"
+
+set JAVA_HOME=%GRAALVM_HOME%
+set PATH=%GRAALVM_HOME%\bin;%PATH%
+
 lein do clean, test
