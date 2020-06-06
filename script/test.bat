828cb2d3a64cd88c7bc0ae2c5057ee9cb63b6916
Michiel Borkent <michielborkent@gmail.com>
2020-06-06 11:54:47 +0200

    wip

--- script/test.bat
+++ script/test.bat
@@ -8,10 +8,4 @@ echo "BABASHKA_TEST_ENV: %BABASHKA_TEST_ENV%"
 set JAVA_HOME=%GRAALVM_HOME%
 set PATH=%GRAALVM_HOME%\bin;%PATH%
 
-echo %PATH%
-
-where java
-java -version
-dir %GRAALVM_HOME%\bin
-
 call lein do clean, test
