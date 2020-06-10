12a5b7b65a557ffc98b6ecae3f02ec08cc04a3f2
Michiel Borkent <michielborkent@gmail.com>
2020-06-10 15:15:59 +0200

    sci: fix instance? for defrecord

--- src/babashka/impl/classes.clj
+++ src/babashka/impl/classes.clj
@@ -222,8 +222,10 @@
     :fields [clojure.lang.PersistentQueue]
     :instance-checks [clojure.lang.IObj
                       clojure.lang.IEditableCollection
+                      clojure.lang.IMapEntry
                       clojure.lang.IPersistentMap
                       clojure.lang.IPersistentVector
+                      clojure.lang.IRecord
                       clojure.lang.ISeq
                       clojure.lang.Named
                       clojure.lang.Keyword
