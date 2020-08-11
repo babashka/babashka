489a630ac38e27e5055a89d5d9f403675d0af92c
Michiel Borkent <michielborkent@gmail.com>
2020-08-11 17:31:22 +0200

    Better

--- src/babashka/impl/datafy.clj
+++ src/babashka/impl/datafy.clj
@@ -1,8 +1,30 @@
 (ns babashka.impl.datafy
   {:no-doc true}
   (:refer-clojure :exclude [create-ns])
-  (:require [babashka.impl.clojure.datafy :as datafy]
+  (:require [clojure.core.protocols :as p]
+            [clojure.datafy :as datafy]
+            [clojure.reflect]
             [sci.core :refer [create-ns copy-var]]))
+
+(defn- sortmap [m]
+  (into (sorted-map) m))
+
+;; Overrides for what is defined in clojure.datafy
+
+(extend-protocol p/Datafiable
+  clojure.lang.Namespace
+  (datafy [n]
+    ;; Override this with nothing, it bloats bb with 30mb and memory usage of GraalVM will peak!
+    #_(with-meta {:name (.getName n)
+                  :publics (-> n ns-publics sortmap)
+                  :imports (-> n ns-imports sortmap)
+                  :interns (-> n ns-interns sortmap)}
+        (meta n)))
+  java.lang.Class
+  (datafy [c]
+    ;; Statically use clojure.reflect instead of leaning on requiring-resolve
+    (let [{:keys [members] :as ret} (clojure.reflect/reflect c)]
+      (assoc ret :name (-> c .getName symbol) :members (->> members (group-by :name) sortmap)))))
 
 (def datafy-ns (create-ns 'clojure.data nil))
 
