2987e78fd1aa6d921a719112847964479303b78d
Michiel Borkent <michielborkent@gmail.com>
2020-08-11 17:40:57 +0200

    wip

--- src/babashka/impl/datafy.clj
+++ src/babashka/impl/datafy.clj
@@ -10,16 +10,16 @@
   (into (sorted-map) m))
 
 ;; Overrides for what is defined in clojure.datafy
-
 (extend-protocol p/Datafiable
   clojure.lang.Namespace
   (datafy [n]
-    ;; Override this with nothing, it bloats bb with 30mb and memory usage of GraalVM will peak!
+    ;; Override this with the default Object implementation. It bloats bb with 30mb and memory usage of GraalVM will peak!
     #_(with-meta {:name (.getName n)
                   :publics (-> n ns-publics sortmap)
                   :imports (-> n ns-imports sortmap)
                   :interns (-> n ns-interns sortmap)}
-        (meta n)))
+        (meta n))
+    n)
   java.lang.Class
   (datafy [c]
     ;; Statically use clojure.reflect instead of leaning on requiring-resolve
