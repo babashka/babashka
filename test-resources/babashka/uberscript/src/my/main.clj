4a25e5db76396b5d5627f588b27cf4672da07f28
Michiel Borkent <michielborkent@gmail.com>
2020-08-17 19:50:12 +0200

    [#535] ignore unresolved symbol in uberscript

--- test-resources/babashka/uberscript/src/my/main.clj
+++ test-resources/babashka/uberscript/src/my/main.clj
@@ -1,6 +1,10 @@
 (ns my.main
-  (:require [my.impl :as impl])
-  (:require [my.impl2 :as impl2]))
+  (:require [my.impl :as impl] ;; my.impl is already loaded, so it will not be loaded again (normally)
+            [my.impl2] ;; but my.impl2 also loads my.impl
+            [my.impl3 :as impl3]))
 
 (defn -main [& args]
+  ;; this function is defined non-top-level and may cause problems
+  (impl3/foo)
+  ;; this should just return args
   (impl/impl-fn args))
