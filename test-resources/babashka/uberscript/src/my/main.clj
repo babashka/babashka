4dd102ca19d212d9591616a65432112d2a0c3f84
Michiel Borkent <michielborkent@gmail.com>
2020-08-17 22:09:44 +0200

    [#538] make uberscript more accepting

--- test-resources/babashka/uberscript/src/my/main.clj
+++ test-resources/babashka/uberscript/src/my/main.clj
@@ -1,10 +1,10 @@
 (ns my.main
   (:require [my.impl :as impl] ;; my.impl is already loaded, so it will not be loaded again (normally)
             [my.impl2] ;; but my.impl2 also loads my.impl
-            [my.impl3 :as impl3]))
+            [my.impl3 :refer [foo]]))
 
 (defn -main [& args]
   ;; this function is defined non-top-level and may cause problems
-  (impl3/foo)
+  (foo)
   ;; this should just return args
   (impl/impl-fn args))
