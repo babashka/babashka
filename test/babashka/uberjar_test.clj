3eaa0d6edf19f64f937eb4f9d0c38f18b45d9d31
Michiel Borkent <michielborkent@gmail.com>
2020-08-19 14:33:40 +0200

    Make tests work

--- test/babashka/uberjar_test.clj
+++ test/babashka/uberjar_test.clj
@@ -9,17 +9,24 @@
   (edn/read-string (apply tu/bb (when (some? input) (str input)) (map str args))))
 
 (deftest uberjar-test
-  (let [tmp-file (java.io.File/createTempFile "uber" ".jar")]
-    #_(.deleteOnExit tmp-file)
+  (let [tmp-file (java.io.File/createTempFile "uber" ".jar")
+        path (.getPath tmp-file)]
+    (.deleteOnExit tmp-file)
     (testing "uberjar"
-      (tu/bb nil "--classpath" "test-resources/babashka/uberjar/src" "-m" "my.main-main" "--uberjar" (.getPath tmp-file))
+      (tu/bb nil "--classpath" "test-resources/babashka/uberjar/src" "-m" "my.main-main" "--uberjar" path)
       (is (= "(\"1\" \"2\" \"3\" \"4\")\n"
-             (tu/bb nil "--jar" (.getPath tmp-file) "1" "2" "3" "4")))
+             (tu/bb nil "--jar" path "1" "2" "3" "4")))
       (is (= "(\"1\" \"2\" \"3\" \"4\")\n"
-             (tu/bb nil "-jar" (.getPath tmp-file) "1" "2" "3" "4")))
+             (tu/bb nil "-jar" path "1" "2" "3" "4")))
       (is (= "(\"1\" \"2\" \"3\" \"4\")\n"
-             (tu/bb nil (.getPath tmp-file) "1" "2" "3" "4"))))
-    (testing "without main, a REPL starts"
-      (tu/bb nil "--classpath" "test-resources/babashka/uberjar/src" "--uberjar" (.getPath tmp-file))
-      ;; TODO: why doesn't this work in the JVM tests?
-      #_(is (str/includes? (tu/bb "(+ 1 2 3)\n" "--jar" (.getPath tmp-file)) "6")))))
+             (tu/bb nil path "1" "2" "3" "4")))))
+  (testing "without main, a REPL starts"
+    ;; NOTE: if we choose the same tmp-file as above and doing this all in the
+    ;; same JVM process, the below test fails because my.main-main will be the
+    ;; main class in the manifest, even if we delete the tmp-file, which may
+    ;; indicate a state-related bug somewhere!
+    (let [tmp-file (java.io.File/createTempFile "uber" ".jar")
+          path (.getPath tmp-file)]
+      (.deleteOnExit tmp-file)
+      (tu/bb nil  "--classpath" "test-resources/babashka/uberjar/src" "--uberjar" path)
+      (is (str/includes? (tu/bb "(+ 1 2 3)" path) "6")))))
