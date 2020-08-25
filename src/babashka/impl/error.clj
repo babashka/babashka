27081a9c1372b18d9c533206bb9d40509455a719
Michiel Borkent <michielborkent@gmail.com>
2020-08-25 11:23:07 +0200

    Refactor error-handling code

---
+++ src/babashka/impl/error.clj
@@ -0,0 +1,124 @@
+(ns babashka.impl.error
+  (:refer-clojure :exclude [error-handler])
+  (:require [babashka.impl.classpath :as cp]
+            [clojure.java.io :as io]
+            [clojure.stacktrace :refer [print-stack-trace]]
+            [clojure.string :as str]
+            [sci.impl.callstack :as cs]))
+
+(defn ruler [title]
+  (println (apply str "----- " title " " (repeat (- 80 7 (count title)) \-))))
+
+(defn split-stacktrace [stacktrace verbose?]
+  (if verbose? [stacktrace]
+      (let [stack-count (count stacktrace)]
+        (if (<= stack-count 10)
+          [stacktrace]
+          [(take 5 stacktrace)
+           (drop (- stack-count 5) stacktrace)]))))
+
+(defn print-stacktrace
+  [stacktrace {:keys [:verbose?]}]
+  (let [stacktrace (cs/format-stacktrace stacktrace)
+        segments (split-stacktrace stacktrace verbose?)
+        [fst snd] segments]
+    (run! println fst)
+    (when snd
+      (println "...")
+      (run! println snd))))
+
+(defn error-context [ex opts]
+  (let [{:keys [:file :line :column]} (ex-data ex)]
+    (when file
+      (when-let [content (case file
+                           "<expr>" (:expression opts)
+                           "<preloads>" (:preloads opts)
+                           (let [f (io/file file)]
+                             (or (when (.exists f) (slurp f))
+                                 (and (not (.isAbsolute f))
+                                      (when-let [loader (:loader opts)]
+                                        (:source (cp/getResource loader [file] nil)))))))]
+        (let [matching-line (dec line)
+              start-line (max (- matching-line 4) 0)
+              end-line (+ matching-line 6)
+              [before after] (->>
+                              (str/split-lines content)
+                              (map-indexed list)
+                              (drop start-line)
+                              (take (- end-line start-line))
+                              (split-at (inc (- matching-line start-line))))
+              snippet-lines (concat before
+                                    [[nil (str (str/join "" (repeat (dec column) " "))
+                                               (str "^--- " (ex-message ex)))]]
+                                    after)
+              indices (map first snippet-lines)
+              max-size (reduce max 0 (map (comp count str) indices))
+              snippet-lines (map (fn [[idx line]]
+                                   (if idx
+                                     (let [line-number (inc idx)]
+                                       (str (format (str "%" max-size "d: ") line-number) line))
+                                     (str (str/join (repeat (+ max-size 2) " ")) line)))
+                                 snippet-lines)]
+          (clojure.string/join "\n" snippet-lines))))))
+
+(defn right-pad [s n]
+  (let [n (- n (count s))]
+    (str s (str/join (repeat n " ")))))
+
+(defn print-locals [locals]
+  (let [max-name-length (reduce max 0 (map (comp count str)
+                                           (keys locals)))
+        max-name-length (+ max-name-length 2)]
+    (binding [*print-length* 10
+              *print-level* 2]
+      (doseq [[k v] locals]
+        (println (str (right-pad (str k ": ") max-name-length) v))))))
+
+(defn error-handler [^Exception e opts]
+  (binding [*out* *err*]
+    (let [d (ex-data e)
+          exit-code (:bb/exit-code d)
+          sci-error? (isa? (:type d) :sci/error)
+          ex-name (when sci-error?
+                    (some-> ^Throwable (ex-cause e)
+                            .getClass .getName))]
+      (if exit-code [nil exit-code]
+          (do
+            (ruler "Error")
+            (println "Type:    " (or
+                                  ex-name
+                                  (.. e getClass getName))
+                     (when-let [t (:type d)]
+                       (str " / " t)))
+            (when-let [m (.getMessage e)]
+              (println (str "Message:  " m)))
+            (let [{:keys [:file :line :column]} d]
+              (when line
+                (println (str "Location: "
+                              (when file (str file ":"))
+                              line ":" column""))))
+            (println)
+            (when-let [ec (when sci-error?
+                            (error-context e opts))]
+              (ruler "Context")
+              (println ec)
+              (println))
+            (when-let [locals (not-empty (:locals d))]
+              (ruler "Locals")
+              (print-locals locals)
+              (println))
+            (when sci-error?
+              (when-let
+                  [st (let [st (with-out-str
+                                 (some->
+                                  d :callstack
+                                  cs/stacktrace
+                                  (print-stacktrace opts)))]
+                        (when-not (str/blank? st) st))]
+                (ruler "Stack trace")
+                (println st)))
+            (when (:verbose? opts)
+              (ruler "Exception")
+              (print-stack-trace e))
+              (flush)
+              [nil 1])))))
--- src/babashka/main.clj
+++ src/babashka/main.clj
@@ -1,5 +1,6 @@
 (ns babashka.main
   {:no-doc true}
+  (:refer-clojure :exclude [error-handler])
   (:require
    [babashka.impl.bencode :refer [bencode-namespace]]
    [babashka.impl.cheshire :refer [cheshire-core-namespace]]
@@ -17,6 +18,7 @@
    [babashka.impl.curl :refer [curl-namespace]]
    [babashka.impl.data :as data]
    [babashka.impl.datafy :refer [datafy-namespace]]
+   [babashka.impl.error :refer [error-handler]]
    [babashka.impl.features :as features]
    [babashka.impl.pods :as pods]
    [babashka.impl.protocols :refer [protocols-namespace]]
@@ -28,12 +30,10 @@
    [babashka.wait :as wait]
    [clojure.edn :as edn]
    [clojure.java.io :as io]
-   [clojure.stacktrace :refer [print-stack-trace]]
    [clojure.string :as str]
    [hf.depstar.uberjar :as uberjar]
    [sci.addons :as addons]
    [sci.core :as sci]
-   [sci.impl.callstack :as cs]
    [sci.impl.namespaces :as sci-namespaces]
    [sci.impl.unrestrict :refer [*unrestricted*]]
    [sci.impl.vars :as vars])
@@ -403,123 +403,6 @@ If neither -e, -f, or --socket-repl are specified, then the first argument that 
   {'java.lang.System/exit exit ;; override exit, so we have more control
    'System/exit exit})
 
-(defn ruler [title]
-  (println (apply str "----- " title " " (repeat (- 80 7 (count title)) \-))))
-
-(defn split-stacktrace [stacktrace verbose?]
-  (if verbose? [stacktrace]
-      (let [stack-count (count stacktrace)]
-        (if (<= stack-count 10)
-          [stacktrace]
-          [(take 5 stacktrace)
-           (drop (- stack-count 5) stacktrace)]))))
-
-(defn print-stacktrace
-  [stacktrace {:keys [:verbose?]}]
-  (let [stacktrace (cs/format-stacktrace stacktrace)
-        segments (split-stacktrace stacktrace verbose?)
-        [fst snd] segments]
-    (run! println fst)
-    (when snd
-      (println "...")
-      (run! println snd))))
-
-(defn error-context [ex opts]
-  (let [{:keys [:file :line :column]} (ex-data ex)]
-    (when file
-      (when-let [content (case file
-                           "<expr>" (:expression opts)
-                           "<preloads>" (:preloads opts)
-                           (let [f (io/file file)]
-                             (or (when (.exists f) (slurp f))
-                                 (and (not (.isAbsolute f))
-                                      (when-let [loader (:loader opts)]
-                                        (:source (cp/getResource loader [file] nil)))))))]
-        (let [matching-line (dec line)
-              start-line (max (- matching-line 4) 0)
-              end-line (+ matching-line 6)
-              [before after] (->>
-                              (clojure.string/split-lines content)
-                              (map-indexed list)
-                              (drop start-line)
-                              (take (- end-line start-line))
-                              (split-at (inc (- matching-line start-line))))
-              snippet-lines (concat before
-                                    [[nil (str (clojure.string/join "" (repeat (dec column) " "))
-                                               (str "^--- " (ex-message ex)))]]
-                                    after)
-              indices (map first snippet-lines)
-              max-size (reduce max 0 (map (comp count str) indices))
-              snippet-lines (map (fn [[idx line]]
-                                   (if idx
-                                     (let [line-number (inc idx)]
-                                       (str (format (str "%" max-size "d: ") line-number) line))
-                                     (str (clojure.string/join (repeat (+ max-size 2) " ")) line)))
-                                 snippet-lines)]
-          (clojure.string/join "\n" snippet-lines))))))
-
-(defn right-pad [s n]
-  (let [n (- n (count s))]
-    (str s (str/join (repeat n " ")))))
-
-(defn print-locals [locals]
-  (let [max-name-length (reduce max 0 (map (comp count str)
-                                           (keys locals)))
-        max-name-length (+ max-name-length 2)]
-    (binding [*print-length* 10
-              *print-level* 2]
-      (doseq [[k v] locals]
-        (println (str (right-pad (str k ": ") max-name-length) v))))))
-
-(defn error-handler* [^Exception e opts]
-  (binding [*out* *err*]
-    (let [d (ex-data e)
-          exit-code (:bb/exit-code d)
-          sci-error? (isa? (:type d) :sci/error)
-          ex-name (when sci-error?
-                    (some-> ^Throwable (ex-cause e)
-                            .getClass .getName))]
-      (if exit-code [nil exit-code]
-          (do
-            (ruler "Error")
-            (println "Type:    " (or
-                                  ex-name
-                                  (.. e getClass getName))
-                     (when-let [t (:type d)]
-                       (str " / " t)))
-            (when-let [m (.getMessage e)]
-              (println (str "Message:  " m)))
-            (let [{:keys [:file :line :column]} d]
-              (when line
-                (println (str "Location: "
-                              (when file (str file ":"))
-                              line ":" column""))))
-            (println)
-            (when-let [ec (when sci-error?
-                            (error-context e opts))]
-              (ruler "Context")
-              (println ec)
-              (println))
-            (when-let [locals (not-empty (:locals d))]
-              (ruler "Locals")
-              (print-locals locals)
-              (println))
-            (when sci-error?
-              (when-let
-                  [st (let [st (with-out-str
-                                 (some->
-                                  d :callstack
-                                  cs/stacktrace
-                                  (print-stacktrace opts)))]
-                        (when-not (str/blank? st) st))]
-                (ruler "Stack trace")
-                (println st)))
-            (when (:verbose? opts)
-              (ruler "Exception")
-              (print-stack-trace e))
-              (flush)
-              [nil 1])))))
-
 (def imports
   '{ArithmeticException java.lang.ArithmeticException
     AssertionError java.lang.AssertionError
@@ -659,7 +542,7 @@ If neither -e, -f, or --socket-repl are specified, then the first argument that 
                                  main)] nil]
                   file (try [[(read-file file)] nil]
                             (catch Exception e
-                              (error-handler* e {:expression expressions
+                              (error-handler e {:expression expressions
                                                  :verbose? verbose?
                                                  :preloads preloads
                                                  :loader (:loader @cp-state)}))))
@@ -672,7 +555,7 @@ If neither -e, -f, or --socket-repl are specified, then the first argument that 
                         (try
                           (sci/eval-string* sci-ctx preloads)
                           (catch Throwable e
-                            (error-handler* e {:expression expression
+                            (error-handler e {:expression expression
                                                :verbose? verbose?
                                                :preloads preloads
                                                :loader (:loader @cp-state)})))))
@@ -714,7 +597,7 @@ If neither -e, -f, or --socket-repl are specified, then the first argument that 
                                      (recur)
                                      res)))))
                            (catch Throwable e
-                             (error-handler* e {:expression expression
+                             (error-handler e {:expression expression
                                                 :verbose? verbose?
                                                 :preloads preloads
                                                 :loader (:loader @cp-state)}))))
