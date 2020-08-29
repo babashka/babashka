fe23683b953c3917bcbfce40f0d37b0b6eb9ed61
Michiel Borkent <michielborkent@gmail.com>
2020-08-29 21:04:50 +0200

    try patch

---
+++ src/babashka/impl/clojure/test/check.clj
@@ -0,0 +1,27 @@
+(ns babashka.impl.clojure.test.check
+  {:no-doc true}
+  (:require [clojure.test.check.random :as r]))
+
+(def next-rng
+  "Returns a random-number generator. Successive calls should return
+  independent results."
+  (let [a (atom (delay (r/make-java-util-splittable-random (System/currentTimeMillis))))
+        thread-local
+        (proxy [ThreadLocal] []
+          (initialValue []
+            (delay
+              (first (split (swap! a #(second (split (deref %)))))))))]
+    (fn []
+      (let [rng @(.get thread-local)
+            [rng1 rng2] (r/split rng)]
+        (.set thread-local (delay rng2))
+        rng1))))
+
+(defn make-random
+  "Given an optional Long seed, returns an object that satisfies the
+  IRandom protocol."
+  ([] (next-rng))
+  ([seed] (r/make-java-util-splittable-random seed)))
+
+(alter-var-root #'r/next-rng (constantly next-rng))
+(alter-var-root #'r/make-random (constantly make-random))
