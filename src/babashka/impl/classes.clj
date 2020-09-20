604dabec8cf495d8fb8c5fa1e1cff78ecf551f42
Michiel Borkent <michielborkent@gmail.com>
2020-09-20 11:38:14 +0200

    [#581] Add java.io.FileNotFoundException

--- src/babashka/impl/classes.clj
+++ src/babashka/impl/classes.clj
@@ -89,6 +89,7 @@
           java.io.ByteArrayOutputStream
           java.io.Console
           java.io.File
+          java.io.FileNotFoundException
           java.io.InputStream
           java.io.IOException
           java.io.OutputStream
