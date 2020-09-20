7742d2e637ab253fd0aab1cee42dc9b565fad85e
Michiel Borkent <michielborkent@gmail.com>
2020-09-20 23:12:18 +0200

    wip

--- feature-httpkit-server/babashka/impl/httpkit_server.clj
+++ feature-httpkit-server/babashka/impl/httpkit_server.clj
@@ -3,9 +3,10 @@
             [sci.core :as sci :refer [copy-var]]))
 
 (def warning
-  "Warning: the org.httpkit.server namespace is experimental and may be removed in future versions of babashka.
+  "WARNING: the org.httpkit.server namespace is experimental and may be removed in future versions of babashka.
 Please leave a note at https://github.com/borkdude/babashka/issues/556 to let us know how you are using it.
-You can turn this warning off using -Dbabashka.httpkit-server.warning=false.")
+You can turn this warning off using -Dbabashka.httpkit-server.warning=false
+This namespace will remain available under a feature flag, see https://github.com/borkdude/babashka/blob/master/doc/build.md#feature-flags")
 
 (def sns (sci/create-ns 'org.httpkit.server
                         {:sci.impl/required-fn (fn [_]
