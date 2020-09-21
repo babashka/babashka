0834fd6ede63a68d92ce3e81ba5d8aa392c957ea
kloimhardt <kloimhardt@kloimhardts-MacBook-Air.local>
2020-09-21 22:29:42 +0200

    replace ring's default-store with GraalVM compatible version

--- feature-ring/babashka/impl/ring_middleware_defaults.clj
+++ feature-ring/babashka/impl/ring_middleware_defaults.clj
@@ -1,6 +1,10 @@
 (ns babashka.impl.ring-middleware-defaults
   (:require [ring.middleware.defaults :as defaults]
+            [ring.middleware.multipart-params]
+            [ring.middleware.multipart-params.temp-file]
             [sci.core :as sci :refer [copy-var]]))
+
+(alter-var-root #'ring.middleware.multipart-params/default-store (constantly (delay ring.middleware.multipart-params.temp-file/temp-file-store)))
 
 (def dns (sci/create-ns 'ring.middleware.defaults nil))
 
