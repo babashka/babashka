62e743c4bad3ba713eb153bd548a73eb87b8db66
kloimhardt <kloimhardt@kloimhardts-MacBook-Air.local>
2020-09-21 22:11:09 +0200

    reitit namespace

---
+++ feature-reitit/babashka/impl/reitit_ring.clj
@@ -0,0 +1,12 @@
+(ns babashka.impl.reitit-ring
+  (:require [reitit.ring :as ring]
+            [sci.core :as sci :refer [copy-var]]))
+
+(def rns (sci/create-ns 'reitit.ring nil))
+
+(def reitit-ring-namespace
+  {:obj rns
+   'ring-handler (copy-var ring/ring-handler rns)
+   'router (copy-var ring/router rns)
+   'routes (copy-var ring/routes rns)
+   'create-resource-handler (copy-var ring/create-resource-handler rns)})
