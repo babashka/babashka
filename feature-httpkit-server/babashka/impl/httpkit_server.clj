820f44cb982ba8a8cb5900ac1a49441141592f90
Chris Badahdah <a.s.badahdah@gmail.com>
2020-09-22 19:30:30 -0700

    Add server-port and send! to org.httpkit.server

--- feature-httpkit-server/babashka/impl/httpkit_server.clj
+++ feature-httpkit-server/babashka/impl/httpkit_server.clj
@@ -17,9 +17,11 @@ This namespace will remain available under a feature flag, see https://github.co
 (def httpkit-server-namespace
   {:obj sns
    'server-stop!              (copy-var server/server-stop! sns)
+   'server-port               (copy-var server/server-port sns)
    'run-server                (copy-var server/run-server sns)
    'sec-websocket-accept      (copy-var server/sec-websocket-accept sns)
    'websocket-handshake-check (copy-var server/websocket-handshake-check sns)
    'send-checked-websocket-handshake! (copy-var server/send-checked-websocket-handshake! sns)
    'send-websocket-handshake! (copy-var server/send-websocket-handshake! sns)
-   'as-channel                (copy-var server/as-channel sns)})
+   'as-channel                (copy-var server/as-channel sns)
+   'send!                     (copy-var server/send! sns)})
