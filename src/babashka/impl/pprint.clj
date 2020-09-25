74d20eafcddfaf08276f3c35db6fff62c3f7b7d5
Michiel Borkent <michielborkent@gmail.com>
2020-09-25 11:29:46 +0200

    hack

--- src/babashka/impl/pprint.clj
+++ src/babashka/impl/pprint.clj
@@ -1,9 +1,51 @@
 (ns babashka.impl.pprint
   {:no-doc true}
-  (:require [babashka.impl.clojure.pprint :as pprint]
+  (:require [clojure.pprint :as pprint]
             [sci.core :as sci]
             [sci.impl.namespaces :refer [copy-var]]
             [sci.impl.vars :as vars]))
+
+(alter-var-root #'pprint/write-option-table
+                (fn [m]
+                  (zipmap (keys m)
+                          (map find-var (vals m)))))
+
+(def new-table-ize
+  (fn [t m]
+    (apply hash-map
+           (mapcat
+            #(when-let [v (get t (key %))] [v (val %)])
+            m))))
+
+(alter-var-root #'pprint/table-ize (constantly new-table-ize))
+
+(alter-meta! #'pprint/with-pretty-writer dissoc :private)
+(alter-meta! #'pprint/pretty-writer? dissoc :private)
+(alter-meta! #'pprint/make-pretty-writer dissoc :private)
+
+(def new-write
+  (fn [object & kw-args]
+    (let [options (merge {:stream true} (apply hash-map kw-args))]
+      (with-bindings (new-table-ize @#'pprint/write-option-table options)
+        (with-bindings
+          (if (or (not (= pprint/*print-base* 10)) pprint/*print-radix*)
+            {#'pr @#'pprint/pr-with-base} {})
+          (let [optval (if (contains? options :stream)
+                         (:stream options)
+                         true)
+                base-writer (condp = optval
+                              nil (java.io.StringWriter.)
+                              true *out*
+                              optval)]
+            (if pprint/*print-pretty*
+              (pprint/with-pretty-writer base-writer
+                (pprint/write-out object))
+              (binding [*out* base-writer]
+                (pr object)))
+            (if (nil? optval)
+              (.toString ^java.io.StringWriter base-writer))))))))
+
+(alter-var-root #'pprint/write (constantly new-write))
 
 (def pprint-ns (vars/->SciNamespace 'clojure.pprint nil))
 
