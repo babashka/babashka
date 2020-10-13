834bd05ac2cc32d1d08bb5644800bdf5d239cf00
Michiel Borkent <michielborkent@gmail.com>
2020-10-13 23:09:53 +0200

    Fix example

--- test/babashka/main_test.clj
+++ test/babashka/main_test.clj
@@ -410,7 +410,7 @@
 (deftest clojure-data-xml-test
   (is (= "<?xml version=\"1.0\" encoding=\"UTF-8\"?><items><item>1</item><item>2</item></items>"
          (bb nil "(let [xml (xml/parse-str \"<items><item>1</item><item>2</item></items>\")] (xml/emit-str xml))")))
-  (is (= "0.0.87-SNAPSHOT" (bb nil "examples/pom_version.clj" (.getPath (io/file "test-resources" "pom.xml"))))))
+  (is (= "0.0.87-SNAPSHOT" (bb nil "examples/get_pom_version.clj" (.getPath (io/file "test-resources" "pom.xml"))))))
 
 (deftest uberscript-test
   (let [tmp-file (java.io.File/createTempFile "uberscript" ".clj")]
