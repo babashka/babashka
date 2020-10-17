5eb489013ec2573f4e19d51deeba8d862cc6c68b
Michiel Borkent <michielborkent@gmail.com>
2020-10-17 23:51:06 +0200

    wip

--- src/babashka/impl/reify.clj
+++ src/babashka/impl/reify.clj
@@ -1,5 +1,48 @@
 (ns babashka.impl.reify
-  {:no-doc true})
+  {:no-doc true}
+  #_(:require [clojure.math.combinatorics :as combo]))
+
+;; (defmacro gen-reify-combos [classes]
+;;   (let [subsets (rest (combo/subsets classes))]
+;;     (reduce (fn [opts classes]
+;;               (assoc opts (set (map (fn [[class _]]
+;;                                       (list 'quote class))
+;;                                     classes))
+;;                      ;; TODO: methods per class
+;;                      `(fn [{:keys [:methods]}]
+;;                         (reify
+;;                           ~@(mapcat
+;;                                 (fn [[clazz methods]]
+;;                                   (cons clazz
+;;                                         (map (fn [[meth args]]
+;;                                                (list meth args
+;;                                                      (list* (list 'get-in 'methods [(list 'quote clazz) (list 'quote meth)])
+;;                                                             args)))
+;;                                              methods)))
+;;                               classes)))))
+;;             {}
+;;             subsets)))
+
+;; (def combos (gen-reify-combos [[clojure.lang.IPending [[isRealized [this]]]]
+;;                                [clojure.lang.IDeref [[deref [this]]]]]))
+
+;; (def pending-and-deref
+;;   (let [factory-fn (get combos '#{clojure.lang.IPending clojure.lang.IDeref})]
+;;     (factory-fn {:methods {'clojure.lang.IPending {'isRealized (fn [_] false)}
+;;                            'clojure.lang.IDeref {'deref (fn [_] :deref)}}})))
+
+;; (prn @pending-and-deref)
+;; (prn (realized? pending-and-deref))
+;; (prn (instance? clojure.lang.IDeref pending-and-deref))
+;; (prn (instance? clojure.lang.IPending pending-and-deref))
+
+;; (def only-deref
+;;   (let [factory-fn (get combos '#{clojure.lang.IDeref})]
+;;     (factory-fn {:methods {'clojure.lang.IDeref {'deref (fn [_] :deref)}}})))
+
+;; (prn @only-deref)
+;; (prn (instance? clojure.lang.IDeref only-deref))
+;; (prn (instance? clojure.lang.IPending only-deref))
 
 (def reify-opts
   {'java.nio.file.FileVisitor
