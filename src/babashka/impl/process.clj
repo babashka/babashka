8c73a02915047e9f5bf8b9f66cfb60fe94b536bb
Michiel Borkent <michielborkent@gmail.com>
2020-10-19 23:46:21 +0200

    [babashka/process#2] *escape-fn*

--- src/babashka/impl/process.clj
+++ src/babashka/impl/process.clj
@@ -5,9 +5,20 @@
 
 (def tns (sci/create-ns 'babashka.process nil))
 
+(def escape-fn (sci/copy-var process/*escape-fn* tns))
+
+(defn process [& args]
+  (binding [process/*escape-fn* @escape-fn]
+    (apply process/process args)))
+
+(defn pb [& args]
+  (binding [process/*escape-fn* @escape-fn]
+    (apply process/pb args)))
+
 (def process-namespace
-  {'process  (copy-var process/process tns)
-   'check    (copy-var process/check tns)
-   'pb       (copy-var process/pb tns)
-   'pipeline (copy-var process/pipeline tns)
-   '$        (copy-var process/$ tns)})
+  {'process     (copy-var process tns)
+   'check       (copy-var process/check tns)
+   'pb          (copy-var pb tns)
+   'pipeline    (copy-var process/pipeline tns)
+   '$           (copy-var process/$ tns)
+   '*escape-fn* escape-fn})
