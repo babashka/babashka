733521ebb083935658522da41c1ef1da5d6220ac
Michiel Borkent <michielborkent@gmail.com>
2020-12-08 22:23:55 +0100

    wip [skip ci]

--- src/babashka/impl/deps.clj
+++ src/babashka/impl/deps.clj
@@ -1,13 +1,19 @@
 (ns babashka.impl.deps
   (:require [babashka.impl.classpath :as cp]
             [borkdude.deps :as deps]
+            [clojure.string :as str]
             [sci.core :as sci]))
 
 (def dns (sci/create-ns 'dns nil))
 
-(defn add-deps [deps-map]
-  (let [cp (with-out-str (deps/-main "-Spath" "-Sdeps" (str {:deps deps-map})))]
-    (cp/add-classpath cp)))
+(defn add-deps
+  ([deps-map] (add-deps deps-map nil))
+  ([deps-map {:keys [:aliases]}]
+   (let [args ["-Spath" "-Sdeps" (str deps-map)]
+         args (cond-> args
+                aliases (conj (str "-A:" (str/join ":" aliases))))
+         cp (with-out-str (apply deps/-main args))]
+     (cp/add-classpath cp))))
 
 (def deps-namespace
   {'add-deps (sci/copy-var add-deps dns)})
