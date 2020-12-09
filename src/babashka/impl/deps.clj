fbe77b9115119e76b21010170edd1ea98020fa80
Michiel Borkent <michielborkent@gmail.com>
2020-12-09 11:44:18 +0100

    wip

--- src/babashka/impl/deps.clj
+++ src/babashka/impl/deps.clj
@@ -6,6 +6,29 @@
 
 (def dns (sci/create-ns 'dns nil))
 
+;;;; merge deps.edn files
+
+(defn- merge-or-replace
+  "If maps, merge, otherwise replace"
+  [& vals]
+  (when (some identity vals)
+    (reduce (fn [ret val]
+              (if (and (map? ret) (map? val))
+                (merge ret val)
+                (or val ret)))
+            nil vals)))
+
+(defn merge-edns
+  "Merge multiple deps edn maps from left to right into a single deps edn map."
+  [deps-edn-maps]
+  (apply merge-with merge-or-replace (remove nil? deps-edn-maps)))
+
+;;;; end merge edn files
+
+;; We are optimizing for the 1-file script with deps scenario where people can
+;; call this function to include e.g. {:deps {medley/medley
+;; {:mvn/version "1.3.3"}}}. Optionally they can include aliases, to modify the
+;; classpath.
 (defn add-deps
   ([deps-map] (add-deps deps-map nil))
   ([deps-map {:keys [:aliases]}]
@@ -15,5 +38,11 @@
          cp (with-out-str (apply deps/-main args))]
      (cp/add-classpath cp))))
 
+(defn clojure
+  ([deps-map & args]
+   (let [args (into ["-Sdeps" (str deps-map)] args)]
+     (apply deps/-main args))))
+
 (def deps-namespace
-  {'add-deps (sci/copy-var add-deps dns)})
+  {'add-deps (sci/copy-var add-deps dns)
+   'clojure (sci/copy-var clojure dns)})
