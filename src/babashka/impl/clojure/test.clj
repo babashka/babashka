4cb62a8be7e96ef947e4bee76ef6b48c01236a2c
Michiel Borkent <michielborkent@gmail.com>
2021-01-02 13:35:38 +0100

    Test cleanup

--- src/babashka/impl/clojure/test.clj
+++ src/babashka/impl/clojure/test.clj
@@ -283,18 +283,6 @@
 
 ;;; UTILITIES FOR REPORTING FUNCTIONS
 
-(defn file-position
-  "Returns a vector [filename line-number] for the nth call up the
-  stack.
-
-  Deprecated in 1.2: The information needed for test reporting is
-  now on :file and :line keys in the result map."
-  {:added "1.1"
-   :deprecated "1.2"}
-  [n]
-  (let [^StackTraceElement s (nth (.getStackTrace (new java.lang.Throwable)) n)]
-    [(.getFileName s) (.getLineNumber s)]))
-
 (defn testing-vars-str
   "Returns a string representation of the current test.  Renders names
   in *testing-vars* as a list, then the source file and line of
