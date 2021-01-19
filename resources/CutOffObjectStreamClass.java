09fcd97b9d41d61b046677d0d0f9964437ee8c71
Michiel Borkent <michielborkent@gmail.com>
2021-01-19 23:24:23 +0100

    Cut off ObjectStreamClass

---
+++ resources/CutOffObjectStreamClass.java
@@ -0,0 +1,12 @@
+import org.graalvm.nativeimage.Platform;
+import org.graalvm.nativeimage.Platforms;
+import com.oracle.svm.core.annotate.TargetClass;
+import com.oracle.svm.core.annotate.Delete;
+
+public final class CutOffObjectStreamClass {
+}
+
+@TargetClass(className = "java.io.ObjectStreamClass")
+@Delete
+final class Target_java_io_ObjectStreamClass {
+}
