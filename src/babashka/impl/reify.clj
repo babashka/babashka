874593bae9cf6c05584364457f0d7bdcc63f1700
Michiel Borkent <michielborkent@gmail.com>
2021-03-06 16:02:41 +0100

    Add clojure.lang.Associative

--- src/babashka/impl/reify.clj
+++ src/babashka/impl/reify.clj
@@ -86,4 +86,7 @@
                               [this a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18]
                               [this a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19]
                               [this a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20]
-                              [this a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 varargs]]}}))
+                              [this a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 varargs]]}
+    clojure.lang.Associative {containsKey [[this k]]
+                              entryAt [[this k]]
+                              assoc [[this k v]]}}))
