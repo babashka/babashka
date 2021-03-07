22daa3bbd5c8ea3c386bc0dd4ec4618a29568dbc
Wilker Lucio <wilkerlucio@gmail.com>
2021-03-07 10:27:55 -0300

    Remove interfaces that were breaking reify compilation

--- src/babashka/impl/reify.clj
+++ src/babashka/impl/reify.clj
@@ -59,14 +59,8 @@
       entryAt     [[this k]]
       assoc       [[this k v]]}
 
-     clojure.lang.Counted
-     {count [[this]]}
-
      clojure.lang.ILookup
      {valAt [[this k] [this k default]]}
-
-     clojure.lang.IMeta
-     {meta [[this]]}
 
      clojure.lang.IFn
      {applyTo [[this arglist]]
@@ -92,9 +86,6 @@
                [this a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19]
                [this a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20]
                [this a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 varargs]]}
-
-     clojure.lang.IObj
-     {withMeta [[this meta]]}
 
      clojure.lang.IPersistentCollection
      {count [[this]]
