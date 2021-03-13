ae047b5809a6710481f8651fb6ee434a3e3384bb
Michiel Borkent <michielborkent@gmail.com>
2021-03-13 14:59:44 +0100

    Support this in proxy

--- src/babashka/impl/proxy.clj
+++ src/babashka/impl/proxy.clj
@@ -12,18 +12,18 @@
   (case (.getName ^Class class)
     "clojure.lang.APersistentMap"
     (proxy [clojure.lang.APersistentMap] []
-      (iterator [] ((method-or-bust methods 'iterator)))
-      (containsKey [k] ((method-or-bust methods 'containsKey) k))
-      (entryAt [k] ((method-or-bust methods 'entryAt) k))
+      (iterator [] ((method-or-bust methods 'iterator) this))
+      (containsKey [k] ((method-or-bust methods 'containsKey) this k))
+      (entryAt [k] ((method-or-bust methods 'entryAt) this k))
       (valAt
-        ([k] ((method-or-bust methods 'valAt) k))
-        ([k default] ((method-or-bust methods 'valAt) k default)))
-      (cons [v] ((method-or-bust methods 'cons) v))
-      (count [] ((method-or-bust methods 'count)))
-      (assoc [k v] ((method-or-bust methods 'assoc) k v))
-      (without [k] ((method-or-bust methods 'without) k))
-      (seq [] ((method-or-bust methods 'seq))))
+        ([k] ((method-or-bust methods 'valAt) this k))
+        ([k default] ((method-or-bust methods 'valAt) this k default)))
+      (cons [v] ((method-or-bust methods 'cons) this v))
+      (count [] ((method-or-bust methods 'count) this))
+      (assoc [k v] ((method-or-bust methods 'assoc) this k v))
+      (without [k] ((method-or-bust methods 'without) this k))
+      (seq [] ((method-or-bust methods 'seq) this)))
     "clojure.lang.AMapEntry"
     (proxy [clojure.lang.AMapEntry] []
-      (key [] ((method-or-bust methods 'key)))
-      (val [] ((method-or-bust methods 'val))))))
+      (key [] ((method-or-bust methods 'key) this))
+      (val [] ((method-or-bust methods 'val) this)))))
