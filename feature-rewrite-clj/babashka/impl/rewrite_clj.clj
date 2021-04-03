a0e94ae567f7cfc0f28cf826dafe360047e83513
Michiel Borkent <michielborkent@gmail.com>
2021-04-03 22:41:13 +0200

    add docs

--- feature-rewrite-clj/babashka/impl/rewrite_clj.clj
+++ feature-rewrite-clj/babashka/impl/rewrite_clj.clj
@@ -37,12 +37,17 @@
 (defn make-ns [ns sci-ns]
   (reduce (fn [ns-map [var-name var]]
             (let [m (meta var)
-                  no-doc (:no-doc m)]
+                  no-doc (:no-doc m)
+                  doc (:doc m)
+                  arglists (:arglists m)]
               (if no-doc ns-map
                   (assoc ns-map var-name
                          (sci/new-var (symbol var-name) @var
-                                      (cond-> {:ns sci-ns}
-                                        (:macro m) (assoc :macro true)))))))
+                                      (cond-> {:ns sci-ns
+                                               :name (:name m)}
+                                        (:macro m) (assoc :macro true)
+                                        doc (assoc :doc doc)
+                                        arglists (assoc :arglists arglists)))))))
           {}
           (ns-publics ns)))
 
