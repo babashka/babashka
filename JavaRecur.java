e33c9f4c35f3c75192796f50753781898c3df9ce
Michiel Borkent <michielborkent@gmail.com>
2021-05-10 16:10:55 +0200

    JavaRecur

--- JavaRecur.java
+++ JavaRecur.java
@@ -1,11 +1,17 @@
 public class JavaRecur {
 
     public static void foo(int x) {
-        System.out.println(x);
+        if (x % 10000 == 0)
+            System.out.println(x);
         foo(x+1);
     }
 
-    public static void main(String [] args) {
-        foo(0);
+    public static void main(String [] args) throws InterruptedException {
+        Runnable runnable =
+            () -> { foo(0); };
+
+        Thread thread = new Thread(null, runnable, "foo", 10000000);
+        thread.start();
+        thread.join();
     }
 }
