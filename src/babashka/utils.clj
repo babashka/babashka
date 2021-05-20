5453a326851fca24d2c826e4581430b1296223aa
Michiel Borkent <michielborkent@gmail.com>
2021-05-20 22:54:34 +0200

    wip

--- src/babashka/utils.clj
+++ src/babashka/utils.clj
@@ -1,9 +1,11 @@
 (ns babashka.utils)
 
-(defmacro when-graal [& body]
-  (when (resolve 'babashka.impl.Graal)
-    `(do ~@body)))
+(defmacro if-graal [then else]
+  (if (resolve 'babashka.impl.Graal)
+    ~then
+    ~else))
 
 (defn set-env [name value]
-  (when-graal
-      (babashka.impl.Graal/setEnv name value)))
+  (if-graal
+      (babashka.impl.Graal/setEnv name value)
+    (throw (UnsupportedOperationException. "set-env is only available in the native image."))))
