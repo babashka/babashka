6e5f210447b82225356b888d3752f21b4f5e10ef
Bob <highpressurecarsalesman@gmail.com>
2021-06-17 12:44:30 -0400

    add more windows testing
    - add `normalize` call to `bb-native` output
    - add `escape-file-paths` function to double up backslashes for Windows paths
      passed as strings
    - add :windows selector to about 10 more tests, along with various small changes
      for Windows testing
    - update test.bat to more closely mirror the Linux/Mac test script

--- script/test.bat
+++ script/test.bat
@@ -8,4 +8,14 @@ echo "BABASHKA_TEST_ENV: %BABASHKA_TEST_ENV%"
 set JAVA_HOME=%GRAALVM_HOME%
 set PATH=%GRAALVM_HOME%\bin;%PATH%
 
+set BABASHKA_PRELOADS=
+set BABASHKA_CLASSPATH=
+set BABASHKA_PRELOADS_TEST=
+
+echo "running tests part 1"
 call lein do clean, test :windows
+
+set BABASHKA_PRELOADS=(defn __bb__foo [] "foo") (defn __bb__bar [] "bar")
+set BABASHKA_PRELOADS_TEST=true
+echo "running tests part 2"
+call lein test :only babashka.main-test/preloads-test
