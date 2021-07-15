8eff1662441b856df8ba4960f6d248ff5f2fe040
Michiel Borkent <michielborkent@gmail.com>
2021-07-15 10:58:36 +0200

    [skip ci]

--- feature-yaml/babashka/impl/yaml.clj
+++ feature-yaml/babashka/impl/yaml.clj
@@ -1,13 +1,15 @@
 (ns babashka.impl.yaml
   {:no-doc true}
   (:require [clj-yaml.core :as yaml]
-            [sci.impl.namespaces :refer [copy-var]]
-            [sci.impl.vars :as vars]))
+            [sci.core :as sci :refer [copy-var]]))
 
-(def yns (vars/->SciNamespace 'clj-yaml.core nil))
+(def yns (sci/create-ns 'clj-yaml.core nil))
 
 (def yaml-namespace
   {'mark (copy-var yaml/mark yns)
    'unmark (copy-var yaml/unmark yns)
    'generate-string (copy-var yaml/generate-string yns)
-   'parse-string (copy-var yaml/parse-string yns)})
+   'parse-string (copy-var yaml/parse-string yns)
+   ;; 'generate-stream (copy-var yaml/generate-stream yns)
+   ;; 'parse-stream (copy-var yaml/parse-stream yns)
+   })
