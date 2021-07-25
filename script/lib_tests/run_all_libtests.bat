2b490542df609b1e0ccc9fbb8acd691084507ead
Bob <highpressurecarsalesman@gmail.com>
2021-07-25 16:15:26 -0400

    fix windows lib tests
    - add lib-tests path to deps.edn and remove it from shell scripts
    - change windows lib test batch script to write a bb.edn file
    - re-enable native lib tests on windows

--- script/lib_tests/run_all_libtests.bat
+++ script/lib_tests/run_all_libtests.bat
@@ -1,5 +1,11 @@
 if "%BABASHKA_TEST_ENV%" EQU "native" (set BB_CMD=.\bb) else (set BB_CMD=lein bb)
 
-for /f %%i in ('.\bb clojure -A:lib-tests -Spath') do set BABASHKA_CLASSPATH=%%i
+set EDN=lib_tests.edn
 
-%BB_CMD% -cp "%BABASHKA_CLASSPATH%;test-resources/lib_tests" -f test-resources/lib_tests/babashka/run_all_libtests.clj %*
+.\bb -f script/lib_tests/bb_edn_from_deps.clj %EDN%
+
+set BABASHKA_EDN=%EDN%
+
+%BB_CMD% -f test-resources/lib_tests/babashka/run_all_libtests.clj %*
+
+set BABASHKA_EDN=
