97749254a9ebfe243addad8dea1df2c36dba721c
Michiel Borkent <michielborkent@gmail.com>
2021-07-25 13:22:24 +0200

    print-deps

---
+++ src/babashka/impl/print_deps.clj
@@ -0,0 +1,25 @@
+(ns babashka.impl.print-deps
+  (:require [babashka.impl.deps :as deps]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.pprint :as pp]))
+
+(defn print-deps [deps-format]
+  (let [deps (-> (io/resource "META-INF/babashka/deps.edn")
+                 slurp
+                 edn/read-string)
+        deps (update deps :deps assoc
+                     'babashka/fs {:mvn/version "0.0.5"}
+                     'babashka/babashka.curl {:mvn/version "0.0.3"})
+        deps (update deps :deps dissoc
+                     'borkdude/sci
+                     'borkdude/graal.locking
+                     'org.postgresql/postgresql
+                     'babashka/clojure-lanterna
+                     'seancorfield/next.jdbc
+                     'datascript/datascript)
+        deps {:deps (:deps deps)}]
+    (case deps-format
+      ("deps" nil) (binding [*print-namespace-maps* false]
+                     (pp/pprint deps))
+      ("classpath") (println (with-out-str (deps/clojure ["-Spath" "-Sdeps" deps]))))))
