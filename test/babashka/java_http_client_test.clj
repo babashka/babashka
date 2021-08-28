795f0f8d15566a067f63498d39fd0a8c9f4ae8fb
Michael Glaesemann <grzm@seespotcode.net>
2021-08-28 16:52:23 -0500

    Add cert tests expired, wrong-host, and self-signed certs. (#982)

--- test/babashka/java_http_client_test.clj
+++ test/babashka/java_http_client_test.clj
@@ -33,3 +33,36 @@
                   (-> (CookieManager.)
                       (.getCookieStore)
                       (.getCookies)))))))
+
+(deftest cert-test
+  (is (= {:expired "java.security.cert.CertificateExpiredException"
+          :wrong-host "sun.security.provider.certpath.SunCertPathBuilderException"
+          :self-signed "sun.security.provider.certpath.SunCertPathBuilderException"}
+         (bb
+           '(do
+              (ns net
+                (:import
+                 [java.net.http HttpClient HttpResponse$BodyHandlers HttpRequest]
+                 [java.net URI]))
+
+              (defn send-and-catch [client req handler]
+                (try
+                  (.send client req (HttpResponse$BodyHandlers/discarding))
+                  (catch Throwable t
+                    (-> (Throwable->map t) :via last :type name))))
+
+              (let [client (-> (HttpClient/newBuilder)
+                               (.build))
+                    handler (HttpResponse$BodyHandlers/discarding)
+                    reqs (->> [:expired :wrong-host :self-signed]
+                              (map (fn [k]
+                                     (let [req (-> (URI. (format "https://%s.badssl.com" (name k)))
+                                                   (HttpRequest/newBuilder)
+                                                   (.GET)
+                                                   (.build))]
+                                       [k req])))
+                              (into {}))]
+                (->> reqs
+                     (map (fn [[k req]]
+                            [k (send-and-catch client req handler)]))
+                     (into {}))))))))
