08e9ec8e311dbdcacc06372d341857e6dc286257
Michiel Borkent <michielborkent@gmail.com>
2021-08-29 13:10:38 +0200

    Add logging for tests

--- test/babashka/java_http_client_test.clj
+++ test/babashka/java_http_client_test.clj
@@ -2,7 +2,9 @@
   (:require
    [babashka.test-utils :as test-utils]
    [clojure.edn :as edn]
-   [clojure.test :as test :refer [deftest is]]))
+   [clojure.template :as tmpl]
+   [clojure.test :as test :refer [deftest is]]
+   [clojure.string :as str]))
 
 (defn bb [expr]
   (edn/read-string (apply test-utils/bb nil [(str expr)])))
@@ -10,84 +12,86 @@
 (deftest java-http-client-test
   (is (= [200 true]
          (bb
-           '(do (ns net
-                  (:import
-                   (java.net.http HttpClient
-                                  HttpRequest
-                                  HttpResponse$BodyHandlers)
-                   (java.net URI)))
+          '(do (ns net
+                 (:import
+                  (java.net.http HttpClient
+                                 HttpRequest
+                                 HttpResponse$BodyHandlers)
+                  (java.net URI)))
 
-                (def req
-                  (-> (HttpRequest/newBuilder (URI. "https://www.clojure.org"))
-                      (.GET)
-                      (.build)))
+               (def req
+                 (-> (HttpRequest/newBuilder (URI. "https://www.clojure.org"))
+                     (.GET)
+                     (.build)))
 
-                (def client
-                  (-> (HttpClient/newBuilder)
-                      (.build)))
+               (def client
+                 (-> (HttpClient/newBuilder)
+                     (.build)))
 
-                (def resp (.send client req (HttpResponse$BodyHandlers/ofString)))
-                [(.statusCode resp) (string? (.body resp))])))))
+               (def resp (.send client req (HttpResponse$BodyHandlers/ofString)))
+               [(.statusCode resp) (string? (.body resp))])))))
 
 (deftest redirect-test
-  (is (= [302 200]
-         (bb
-           '(do
-              (ns net
-                (:import
-                 (java.net.http HttpClient
-                                HttpClient$Redirect
-                                HttpRequest
-                                HttpRequest$BodyPublishers
-                                HttpResponse$BodyHandlers)
-                 (java.net URI)))
-              (defn log [x] (.println System/err x))
-              (let [req (-> (HttpRequest/newBuilder (URI. "https://www.postman-echo.com"))
-                            (.GET)
-                            (.timeout (java.time.Duration/ofSeconds 5))
-                            (.build))
-                    never-redirecting-client (-> (HttpClient/newBuilder)
-                                                 (.followRedirects HttpClient$Redirect/NEVER)
-                                                 (.build))
-                    redirecting-client (-> (HttpClient/newBuilder)
-                                           (.followRedirects HttpClient$Redirect/ALWAYS)
-                                           (.build))
-                    handler (HttpResponse$BodyHandlers/discarding)]
-                [(do
-                   (log "doing never redirect")
-                   (.statusCode (.send never-redirecting-client req handler)))
-                 (do
-                   (log "doing redirect")
-                   (.statusCode (.send redirecting-client req handler)))]))))))
+  (let [redirect-prog
+        (fn [redirect-kind]
+          (str/replace (str '(do
+                               (ns net
+                                 (:import
+                                  (java.net.http HttpClient
+                                                 HttpClient$Redirect
+                                                 HttpRequest
+                                                 HttpRequest$BodyPublishers
+                                                 HttpResponse$BodyHandlers)
+                                  (java.net URI)))
+                               (defn log [x] (.println System/err x))
+                               (let [req (-> (HttpRequest/newBuilder (URI. "https://www.postman-echo.com"))
+                                             (.GET)
+                                             (.timeout (java.time.Duration/ofSeconds 5))
+                                             (.build))
+                                     client (-> (HttpClient/newBuilder)
+                                                (.followRedirects :redirect/kind)
+                                                (.build))
+                                     handler (HttpResponse$BodyHandlers/discarding)]
+                                 (.statusCode (.send client req handler)))))
+                       ":redirect/kind"
+                       (case redirect-kind
+                         :never
+                         "HttpClient$Redirect/NEVER"
+                         :always
+                         "HttpClient$Redirect/ALWAYS")))]
+    (println "Testing redirect always")
+    (is (= 200 (bb (redirect-prog :always))))
+    (println "Testing redirect never")
+    (is (= 302 (bb (redirect-prog :never))))))
 
 (deftest post-input-stream-test
   (let [body "with love from java.net.http"]
     (is (= body
            (bb
-             '(do
-                (ns net
-                  (:require
-                   [cheshire.core :as json]
-                   [clojure.java.io :as io])
-                  (:import
-                   (java.net.http HttpClient
-                                  HttpRequest
-                                  HttpRequest$BodyPublishers
-                                  HttpResponse$BodyHandlers)
-                   (java.net URI)
-                   (java.util.function Supplier)))
-                (let [body "with love from java.net.http"
-                      req (-> (HttpRequest/newBuilder (URI. "https://www.postman-echo.com/post"))
-                              (.method "POST" (HttpRequest$BodyPublishers/ofInputStream
-                                                (reify Supplier (get [_]
-                                                                  (io/input-stream (.getBytes body))))))
-                              (.build))
-                      client (-> (HttpClient/newBuilder)
-                                 (.build))
-                      res (.send client req (HttpResponse$BodyHandlers/ofString))]
-                  (-> (.body res)
-                      (json/parse-string true)
-                      :data))))))))
+            '(do
+               (ns net
+                 (:require
+                  [cheshire.core :as json]
+                  [clojure.java.io :as io])
+                 (:import
+                  (java.net.http HttpClient
+                                 HttpRequest
+                                 HttpRequest$BodyPublishers
+                                 HttpResponse$BodyHandlers)
+                  (java.net URI)
+                  (java.util.function Supplier)))
+               (let [body "with love from java.net.http"
+                     req (-> (HttpRequest/newBuilder (URI. "https://www.postman-echo.com/post"))
+                             (.method "POST" (HttpRequest$BodyPublishers/ofInputStream
+                                              (reify Supplier (get [_]
+                                                                (io/input-stream (.getBytes body))))))
+                             (.build))
+                     client (-> (HttpClient/newBuilder)
+                                (.build))
+                     res (.send client req (HttpResponse$BodyHandlers/ofString))]
+                 (-> (.body res)
+                     (json/parse-string true)
+                     :data))))))))
 
 (deftest cookie-test
   (is (= []
@@ -123,28 +127,28 @@
 (deftest authenticator-test
   (is (= [401 200]
          (bb
-           '(do
-              (ns net
-                (:import
-                 (java.net Authenticator
-                           PasswordAuthentication
-                           URI)
-                 (java.net.http HttpClient
-                                HttpRequest
-                                HttpResponse$BodyHandlers)))
-              (let [no-auth-client (-> (HttpClient/newBuilder)
-                                       (.build))
-                    req (-> (HttpRequest/newBuilder (URI. "https://www.postman-echo.com/basic-auth"))
-                            (.build))
-                    handler (HttpResponse$BodyHandlers/discarding)
-                    no-auth-res (.send no-auth-client req handler)
-                    authenticator (proxy [Authenticator] []
-                                    (getPasswordAuthentication [] (PasswordAuthentication. "postman" (char-array "password"))))
-                    auth-client (-> (HttpClient/newBuilder)
-                                    (.authenticator authenticator)
-                                    (.build))
-                    auth-res (.send auth-client req handler)]
-                [(.statusCode no-auth-res) (.statusCode auth-res)]))))))
+          '(do
+             (ns net
+               (:import
+                (java.net Authenticator
+                          PasswordAuthentication
+                          URI)
+                (java.net.http HttpClient
+                               HttpRequest
+                               HttpResponse$BodyHandlers)))
+             (let [no-auth-client (-> (HttpClient/newBuilder)
+                                      (.build))
+                   req (-> (HttpRequest/newBuilder (URI. "https://www.postman-echo.com/basic-auth"))
+                           (.build))
+                   handler (HttpResponse$BodyHandlers/discarding)
+                   no-auth-res (.send no-auth-client req handler)
+                   authenticator (proxy [Authenticator] []
+                                   (getPasswordAuthentication [] (PasswordAuthentication. "postman" (char-array "password"))))
+                   auth-client (-> (HttpClient/newBuilder)
+                                   (.authenticator authenticator)
+                                   (.build))
+                   auth-res (.send auth-client req handler)]
+               [(.statusCode no-auth-res) (.statusCode auth-res)]))))))
 
 (deftest cert-test
   (is (= {:expired "java.security.cert.CertificateExpiredException"
@@ -153,37 +157,37 @@
           :untrusted-root "sun.security.provider.certpath.SunCertPathBuilderException"
           :wrong-host "sun.security.provider.certpath.SunCertPathBuilderException"}
          (bb
-           '(do
-              (ns net
-                (:import
-                 (java.net URI)
-                 (java.net.http HttpClient
-                                HttpRequest
-                                HttpResponse$BodyHandlers)))
+          '(do
+             (ns net
+               (:import
+                (java.net URI)
+                (java.net.http HttpClient
+                               HttpRequest
+                               HttpResponse$BodyHandlers)))
 
-              (defn send-and-catch [client req handler]
-                (try
-                  (let [res (.send client req (HttpResponse$BodyHandlers/discarding))]
-                    (.statusCode res))
-                  (catch Throwable t
-                    (-> (Throwable->map t) :via last :type name))))
+             (defn send-and-catch [client req handler]
+               (try
+                 (let [res (.send client req (HttpResponse$BodyHandlers/discarding))]
+                   (.statusCode res))
+                 (catch Throwable t
+                   (-> (Throwable->map t) :via last :type name))))
 
-              (let [client (-> (HttpClient/newBuilder)
-                               (.build))
-                    handler (HttpResponse$BodyHandlers/discarding)
-                    reqs (->> [:expired
-                               :self-signed
-                               :revoked
-                               :untrusted-root
-                               :wrong-host]
-                              (map (fn [k]
-                                     (let [req (-> (URI. (format "https://%s.badssl.com" (name k)))
-                                                   (HttpRequest/newBuilder)
-                                                   (.GET)
-                                                   (.build))]
-                                       [k req])))
-                              (into {}))]
-                (->> reqs
-                     (map (fn [[k req]]
-                            [k (send-and-catch client req handler)]))
-                     (into {}))))))))
+             (let [client (-> (HttpClient/newBuilder)
+                              (.build))
+                   handler (HttpResponse$BodyHandlers/discarding)
+                   reqs (->> [:expired
+                              :self-signed
+                              :revoked
+                              :untrusted-root
+                              :wrong-host]
+                             (map (fn [k]
+                                    (let [req (-> (URI. (format "https://%s.badssl.com" (name k)))
+                                                  (HttpRequest/newBuilder)
+                                                  (.GET)
+                                                  (.build))]
+                                      [k req])))
+                             (into {}))]
+               (->> reqs
+                    (map (fn [[k req]]
+                           [k (send-and-catch client req handler)]))
+                    (into {}))))))))
