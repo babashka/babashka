0428d983d9549e5855c621fd0b2d6f08f77611e1
Michiel Borkent <michielborkent@gmail.com>
2021-11-19 21:46:16 +0100

    update to newest sci

--- src/babashka/impl/fs.clj
+++ src/babashka/impl/fs.clj
@@ -1,9 +1,6 @@
 (ns babashka.impl.fs
-  (:require [babashka.fs :as fs]
+  (:require [babashka.fs]
             [sci.core :as sci]))
 
-(def current-file (fs/file (fs/absolutize (fs/path "src" *file*))))
-
 (def fs-namespace
-  (sci/copy-ns 'babashka.fs (sci/create-ns 'babashka.fs)))
-
+  (sci/copy-ns babashka.fs (sci/create-ns 'babashka.fs)))
