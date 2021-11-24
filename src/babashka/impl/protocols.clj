398dd2c0c37038fe7edd11d6fe2344405a520063
Michiel Borkent <michielborkent@gmail.com>
2021-11-24 11:21:54 +0100

    IKVReduce

--- src/babashka/impl/protocols.clj
+++ src/babashka/impl/protocols.clj
@@ -1,5 +1,6 @@
 (ns babashka.impl.protocols
-  (:require [clojure.datafy :as d]
+  (:require [clojure.core.protocols :as p]
+            [clojure.datafy :as d]
             [sci.core :as sci :refer [copy-var]]
             [sci.impl.types :as types]))
 
@@ -26,14 +27,32 @@
   ;; note: Clojure itself will handle checking metadata for impls
   (d/nav coll k v))
 
+;;;; IKVreduce
+
+(defmulti kv-reduce types/type-impl)
+
+(defmethod kv-reduce :sci.impl.protocols/reified [amap f init]
+  (let [methods (types/getMethods amap)]
+    ((get methods 'kv-reduce) amap f init)))
+
+(defmethod kv-reduce :default [amap f init]
+  (p/kv-reduce amap f init))
+
 ;;;; sci namespace
 (def protocols-ns (sci/create-ns 'clojure.core.protocols nil))
 
 (def protocols-namespace
-  {'Datafiable (sci/new-var 'clojure.core.protocols/Datafiable {:methods #{'datafy}
+  {;; Datafiable
+   'Datafiable (sci/new-var 'clojure.core.protocols/Datafiable {:methods #{'datafy}
                                                                 :ns protocols-ns} {:ns protocols-ns})
    'datafy (copy-var datafy protocols-ns)
+
+   ;; Navigable
    'Navigable (sci/new-var 'clojure.core.protocols/Navigable {:methods #{'nav}
                                                               :ns protocols-ns} {:ns protocols-ns})
    'nav (copy-var nav protocols-ns)
-   'IKVReduce (copy-var datafy protocols-ns)})
+
+   ;; IKVReduce
+   'IKVReduce (sci/new-var 'clojure.core.protocols/IKVReduce {:methods #{'kv-reduce}
+                                                              :ns protocols-ns} {:ns protocols-ns})
+   'kv-reduce (copy-var kv-reduce protocols-ns)})
