a98fe1b4a5f5dbda268532ecee26d3e9fc6419df
Michiel Borkent <michielborkent@gmail.com>
2021-11-24 16:11:27 +0100

    order of loading

--- src/aaaa_this_has_to_be_first/because_patches.clj
+++ src/aaaa_this_has_to_be_first/because_patches.clj
@@ -1,4 +1,5 @@
 (ns aaaa-this-has-to-be-first.because-patches
   ;; we need pprint loaded first, it patches pprint to not bloat the GraalVM binary
-  (:require [babashka.impl.datafy] ;; overrides impl for clojure.lang.Namespace
-            [babashka.impl.pprint]))
+  (:require [babashka.impl.pprint]
+            [babashka.impl.protocols] ;; overrides impl for clojure.lang.Namespace
+            ))
--- src/babashka/impl/protocols.clj
+++ src/babashka/impl/protocols.clj
@@ -1,8 +1,16 @@
 (ns babashka.impl.protocols
-  (:require [clojure.core.protocols :as p]
+  (:require [babashka.impl.common :refer [ctx]]
+            [babashka.impl.protocols :as protocols]
+            [clojure.core.protocols :as p]
             [clojure.datafy :as d]
+            ;; ensure datafy is loaded, we're going to override its
+            ;; clojure.lang.Namespace implementation for datafy
+            [clojure.reflect]
             [sci.core :as sci :refer [copy-var]]
-            [sci.impl.types :as types]))
+            [sci.impl.namespaces :refer [sci-ns-name sci-ns-publics sci-ns-imports sci-ns-interns]]
+            [sci.impl.types :as types]
+            [sci.impl.vars])
+  (:import [sci.impl.vars SciNamespace]))
 
 ;;;; datafy
 
@@ -39,6 +47,37 @@
 ;;   (p/kv-reduce amap f init))
 
 ;;;; sci namespace
+
+;; Overrides for what is defined in clojure.datafy
+
+(defn- sortmap [m]
+  (into (sorted-map) m))
+
+(extend-protocol p/Datafiable
+  clojure.lang.Namespace
+  (datafy [n]
+    ;; Override this with the default Object implementation. It bloats bb with 30mb and memory usage of GraalVM will peak!
+    #_(with-meta {:name (.getName n)
+                  :publics (-> n ns-publics sortmap)
+                  :imports (-> n ns-imports sortmap)
+                  :interns (-> n ns-interns sortmap)}
+        (meta n))
+    n)
+  java.lang.Class
+  (datafy [c]
+    ;; Statically use clojure.reflect instead of leaning on requiring-resolve
+    (let [{:keys [members] :as ret} (clojure.reflect/reflect c)]
+      (assoc ret :name (-> c .getName symbol) :members (->> members (group-by :name) sortmap)))))
+
+(extend-protocol p/Datafiable
+  SciNamespace
+  (datafy [n]
+    (with-meta {:name (sci-ns-name n)
+                :publics (->> n (sci-ns-publics @ctx) sortmap)
+                :imports (->> n (sci-ns-imports @ctx) sortmap)
+                :interns (->> n (sci-ns-interns @ctx) sortmap)}
+      (meta n))))
+
 (def protocols-ns (sci/create-ns 'clojure.core.protocols nil))
 
 (def protocols-namespace
