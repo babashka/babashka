557df762f33f2bec5d344c6d070209732e5ce83b
Michiel Borkent <michielborkent@gmail.com>
2021-11-24 17:23:44 +0100

    xform support

--- src/babashka/impl/protocols.clj
+++ src/babashka/impl/protocols.clj
@@ -1,16 +1,13 @@
 (ns babashka.impl.protocols
-  (:require [babashka.impl.common :refer [ctx]]
-            [babashka.impl.protocols :as protocols]
+  (:require [babashka.impl.protocols :as protocols]
             [clojure.core.protocols :as p]
             [clojure.datafy :as d]
             ;; ensure datafy is loaded, we're going to override its
             ;; clojure.lang.Namespace implementation for datafy
             [clojure.reflect]
             [sci.core :as sci :refer [copy-var]]
-            [sci.impl.namespaces :refer [sci-ns-name sci-ns-publics sci-ns-imports sci-ns-interns]]
             [sci.impl.types :as types]
-            [sci.impl.vars])
-  (:import [sci.impl.vars SciNamespace]))
+            [sci.impl.vars]))
 
 ;;;; datafy
 
@@ -34,49 +31,6 @@
 (defmethod nav :default [coll k v]
   ;; note: Clojure itself will handle checking metadata for impls
   (d/nav coll k v))
-
-;;;; IKVreduce
-
-;; (defmulti kv-reduce types/type-impl)
-
-;; (defmethod kv-reduce :sci.impl.protocols/reified [amap f init]
-;;   (let [methods (types/getMethods amap)]
-;;     ((get methods 'kv-reduce) amap f init)))
-
-;; (defmethod kv-reduce :default [amap f init]
-;;   (p/kv-reduce amap f init))
-
-;;;; sci namespace
-
-;; Overrides for what is defined in clojure.datafy
-
-(defn- sortmap [m]
-  (into (sorted-map) m))
-
-(extend-protocol p/Datafiable
-  clojure.lang.Namespace
-  (datafy [n]
-    ;; Override this with the default Object implementation. It bloats bb with 30mb and memory usage of GraalVM will peak!
-    #_(with-meta {:name (.getName n)
-                  :publics (-> n ns-publics sortmap)
-                  :imports (-> n ns-imports sortmap)
-                  :interns (-> n ns-interns sortmap)}
-        (meta n))
-    n)
-  java.lang.Class
-  (datafy [c]
-    ;; Statically use clojure.reflect instead of leaning on requiring-resolve
-    (let [{:keys [members] :as ret} (clojure.reflect/reflect c)]
-      (assoc ret :name (-> c .getName symbol) :members (->> members (group-by :name) sortmap)))))
-
-(extend-protocol p/Datafiable
-  SciNamespace
-  (datafy [n]
-    (with-meta {:name (sci-ns-name n)
-                :publics (->> n (sci-ns-publics @ctx) sortmap)
-                :imports (->> n (sci-ns-imports @ctx) sortmap)
-                :interns (->> n (sci-ns-interns @ctx) sortmap)}
-      (meta n))))
 
 (def protocols-ns (sci/create-ns 'clojure.core.protocols nil))
 
