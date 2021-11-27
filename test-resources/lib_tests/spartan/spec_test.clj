a40c97128f9af0a7a9395c3e114a0497aa0b963a
Michiel Borkent <michielborkent@gmail.com>
2021-11-27 14:53:57 +0100

    fix spec tests

--- test-resources/lib_tests/spartan/spec_test.clj
+++ test-resources/lib_tests/spartan/spec_test.clj
@@ -1,8 +1,8 @@
 (ns spartan.spec-test
   (:require [clojure.test :as t :refer [deftest is]]))
 
-(time (require '[spartan.spec :as s]))
-(require '[spartan.spec :as s])
+(time (require '[spartan.spec]))
+(require '[clojure.spec.alpha :as s])
 
 (deftest spec-test
   (time (s/explain (s/cat :i int? :s string?) [1 :foo]))
