af24962a8e58a9f104c0848edd7a2c6fcec63e45
Michiel Borkent <michielborkent@gmail.com>
2022-04-10 21:33:00 +0200

    wip [skip ci]

--- project.clj
+++ project.clj
@@ -28,7 +28,7 @@
                  [org.clojure/test.check "1.1.1"]
                  [com.github.clj-easy/graal-build-time "0.1.0"]
                  [rewrite-clj/rewrite-clj "1.0.699-alpha"]
-                 [insn/insn {:mvn/version "0.5.2"}]]
+                 [insn/insn "0.5.2"]]
   :plugins       [[org.kipz/lein-meta-bom "0.1.1"]]
   :metabom {:jar-name "metabom.jar"}
   :profiles {:feature/xml  {:source-paths ["feature-xml"]
