8254c2fb4116d5b86331f8f8c9ee44a676c2bd99
Michiel Borkent <michielborkent@gmail.com>
2022-04-13 12:25:27 +0200

    Add reify lib

---
+++ reify/build.clj
@@ -0,0 +1,34 @@
+(ns build
+  (:require [babashka.impl.reify2 :as reify2]
+            [clojure.tools.build.api :as b]))
+
+(def lib 'org.babashka/babashka.impl.reify)
+(def version "0.0.1")
+(def class-dir "target/classes")
+(def basis (b/create-basis {:project "deps.edn"}))
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(defn clean [_]
+  (b/delete {:path "target"}))
+
+(defn gen-classes [_]
+  (reify2/gen-classes nil))
+
+(defn jar [_]
+  (gen-classes nil)
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis basis
+                :src-dirs ["src"]})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
+
+(defn deploy [opts]
+  (jar opts)
+  ((requiring-resolve 'deps-deploy.deps-deploy/deploy)
+    (merge {:installer :remote
+                       :artifact jar-file
+                       :pom-file (b/pom-path {:lib lib :class-dir class-dir})}
+                    opts))
+  opts)
