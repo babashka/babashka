cc2c682e79f3252b7ee97cecb82515f1ef41ba65
Michiel Borkent <michielborkent@gmail.com>
2022-06-19 22:41:52 +0200

    SCI now has memfn

--- src/babashka/impl/clojure/core.clj
+++ src/babashka/impl/clojure/core.clj
@@ -195,7 +195,6 @@
    'iteration (sci/copy-var iteration clojure-core-ns)
    'abs (sci/copy-var abs clojure-core-ns)
    'StackTraceElement->vec (sci/copy-var StackTraceElement->vec clojure-core-ns)
-   'memfn (sci/copy-var memfn clojure-core-ns)
    'into-array (sci/copy-var into-array clojure-core-ns)
    'print-method (sci/copy-var print-method clojure-core-ns)
    'print-dup (sci/copy-var print-dup clojure-core-ns)}
