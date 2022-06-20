7f53e0146243643b4521fb582cb39a3933edd250
Rahul De <rahul080327@gmail.com>
2022-06-20 20:33:30 +0100

    [skip ci] add branch filter back to docker

--- .circleci/script/short_ci.clj
+++ .circleci/script/short_ci.clj
@@ -205,7 +205,8 @@ java -jar \"$jar\" --config .build/bb.edn --deps-root . release-artifact \"$refl
                                     "linux-aarch64-static"
                                     {:deploy {:filters  {:branches {:only "master"}}
                                               :requires ["jvm" "linux"]}}
-                                    {:docker {:requires ["linux" "linux-static" "linux-aarch64"]}}]}))))
+                                    {:docker {:filters  {:branches {:only "master"}}
+                                              :requires ["linux" "linux-static" "linux-aarch64"]}}]}))))
 
 (def skip-config
   {:skip-if-only [#".*.md$"]})
