d9062aa1256d48cfa72f3191da8c3db786900f5e
Rahul De <rahul080327@gmail.com>
2022-06-20 20:21:06 +0100

    [wip] pass arch only when aarch64

--- .circleci/script/short_ci.clj
+++ .circleci/script/short_ci.clj
@@ -107,8 +107,10 @@ java -jar \"$jar\" --config .build/bb.edn --deps-root . release-artifact \"$refl
                                                "macos"
                                                platform)
                           :BABASHKA_TEST_ENV "native"
-                          :BABASHKA_ARCH     arch
                           :BABASHKA_XMX      "-J-Xmx6500m"}
+        env              (if (= "aarch64" arch)
+                           (assoc env :BABASHKA_ARCH arch)
+                           env)
         env              (if static?
                            (assoc env :BABASHKA_STATIC "true")
                            env)
