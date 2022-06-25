72e9b6ceee2160be1f48700ad81dbc2ea2880999
Michiel Borkent <michielborkent@gmail.com>
2022-06-25 21:21:27 +0200

    process update

--- src/babashka/impl/nrepl_server.clj
+++ src/babashka/impl/nrepl_server.clj
@@ -1,16 +1,16 @@
 (ns babashka.impl.nrepl-server
   {:no-doc true}
   (:require [babashka.impl.common :as common]
-            [babashka.nrepl.server :refer [start-server! stop-server!]]
-            [sci.core :refer [create-ns new-var copy-var]]))
+            [babashka.nrepl.server :as server]
+            [sci.core :as sci]))
+
+(defn start-server!
+  ([]
+   (server/start-server! @common/ctx))
+  ([opts]
+   (server/start-server! @common/ctx opts)))
 
 (def nrepl-server-namespace
   (let [ns-sci (create-ns 'babashka.nrepl.server)]
-    {'start-server! (new-var 'start-server!
-                             (fn
-                               ([]
-                                (start-server! @common/ctx))
-                               ([opts]
-                                (start-server! @common/ctx opts)))
-                             {:ns ns-sci})
-     'stop-server! (copy-var stop-server! ns-sci)}))
+    {'start-server! (sci/copy-var start-server! ns-sci)
+     'stop-server! (sci/copy-var server/stop-server! ns-sci)}))
