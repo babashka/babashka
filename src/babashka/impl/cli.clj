de439171402814ed2bc9532de28a3917dd25c0ca
Michiel Borkent <michielborkent@gmail.com>
2022-07-31 13:42:44 +0200

    the-var

--- src/babashka/impl/cli.clj
+++ src/babashka/impl/cli.clj
@@ -12,17 +12,17 @@
   (format "
 (do
 (require '[babashka.cli])
-(let [var (requiring-resolve `%1$s)
-      var-meta (meta var)
-      ns (:ns (meta var))
+(let [the-var (requiring-resolve `%1$s)
+      the-var-meta (meta the-var)
+      ns (:ns (meta the-var))
       ns-meta (meta ns)
       ct (babashka.tasks/current-task)
       cli-opts (babashka.cli/merge-opts (:org.babashka/cli ns-meta)
-                                        (:org.babashka/cli var-meta)
+                                        (:org.babashka/cli the-var-meta)
                                         (:org.babashka/cli ct))
       opts (babashka.cli/parse-opts *command-line-args* cli-opts)
       task-exec-args (:exec-args ct)
       cli-exec-args (:exec-args cli-opts)
       opts (babashka.cli/merge-opts cli-exec-args task-exec-args opts)]
-(var opts)))"
+(the-var opts)))"
           sym))
