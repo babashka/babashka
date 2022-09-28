6e4b363711b4bc9f31fb26e4fc6f519193f663af
Rahul De <rahul080327@gmail.com>
2022-09-28 19:15:40 +0530

    print with binding

--- src/babashka/main.clj
+++ src/babashka/main.clj
@@ -1049,7 +1049,8 @@ Use bb run --help to show this help output.
        (catch java.lang.RuntimeException e
          (if (re-find #"No dispatch macro for: \"" (.getMessage e))
            (throw (ex-info "Invalid regex literal found in EDN config, use re-pattern instead" {}))
-           (do (.println *err* "Error during loading bb.edn:")
+           (do (binding [*out* *err*]
+                 (println "Error during loading bb.edn:"))
                (throw e))))))
 
 (defn main [& args]
