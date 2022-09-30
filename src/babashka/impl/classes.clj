94e0d2d0ac4d6d7ef158c437a786527d4c721284
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-09-30 11:20:14 -0400

    wip

--- src/babashka/impl/classes.clj
+++ src/babashka/impl/classes.clj
@@ -471,6 +471,7 @@
           java.util.zip.ZipFile
           sun.misc.Signal
           sun.misc.SignalHandler
+          sun.util.calendar.ZoneInfo
           ~(symbol "[B")
           ~(symbol "[I")
           ~(symbol "[Ljava.lang.Object;")
