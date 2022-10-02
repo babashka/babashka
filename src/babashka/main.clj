7e7bb5f5403ac387965f0e530c3c9d3e3f652a2d
Michiel Borkent <michielborkent@gmail.com>
2022-10-02 12:39:18 +0200

    wip

--- src/babashka/main.clj
+++ src/babashka/main.clj
@@ -854,7 +854,7 @@ Use bb run --help to show this help output.
                                    {})
                                res)))
                          (case namespace
-                           clojure.spec.alpha
+                           #_#_clojure.spec.alpha
                            (binding [*out* *err*]
                              (println "[babashka] WARNING: Use the babashka-compatible version of clojure.spec.alpha, available here: https://github.com/babashka/spec.alpha"))
                            clojure.core.specs.alpha
