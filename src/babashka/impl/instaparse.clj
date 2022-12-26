568b8923eedac011e60608090467e4bafd6f67f0
Michiel Borkent <michielborkent@gmail.com>
2022-12-26 12:52:07 +0100

    instaparse [skip ci]

--- src/babashka/impl/instaparse.clj
+++ src/babashka/impl/instaparse.clj
@@ -1,15 +1,16 @@
 (ns babashka.impl.instaparse
-  (:require [instaparse.core :as insta]
-            [instaparse.combinators-source :as source]
-            [sci.core :as sci]))
+  (:require
+   [instaparse.combinators-source :as source]
+   [instaparse.core :as insta]
+   [sci.core :as sci]))
 
 (def ins (sci/create-ns 'instaparse.core))
 
 (def instaparse-namespace
-  {'defparser (sci/copy-var insta/defparser ins)
-   'map->Parser (sci/copy-var insta/map->Parser ins)
+  {#_#_'defparser (sci/copy-var insta/defparser ins)
+   #_#_'map->Parser (sci/copy-var insta/map->Parser ins)
    'parser (sci/copy-var insta/parser ins)
-   'transform (sci/copy-var insta/transform ins)})
+   #_#_'transform (sci/copy-var insta/transform ins)})
 
 (def sns (sci/create-ns 'instaparse.combinators-source))
 
