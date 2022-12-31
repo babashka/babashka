b752d8f1571656a6fc62f63c5ca958169a3adcb7
Rahul De <rahul080327@gmail.com>
2022-12-31 10:49:57 +0000

    tolerate subversions

--- script/check_glibc.sh
+++ script/check_glibc.sh
@@ -6,6 +6,6 @@ function ver_lte() {
 
 max_glibc_version="2.31"
 
-current_glibc_version=$(ldd --version | head -1 | awk '{print $4}')
+current_glibc_version=$(ldd --version | head -1 | awk '{print $4}' | cut -d "-" -f 1)
 
 ver_lte "${current_glibc_version}" "${max_glibc_version}" || echo "glibc greater than max version ${max_glibc_version}: ${current_glibc_version}"; exit 1
