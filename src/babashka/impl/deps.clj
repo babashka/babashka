3fa92a6335138f8dc2ca6fc82fa8992912a1b275
Rahul De <rahul080327@gmail.com>
2023-01-27 14:10:18 +0000

    [#1473] Make deps resolve paths relative to bb.edn

--- src/babashka/impl/deps.clj
+++ src/babashka/impl/deps.clj
@@ -89,8 +89,11 @@
                             "-Sdeps-file" "") ;; we reset deps file so the local deps.edn isn't used
                  args (if force (cons "-Sforce" args) args)
                  args (concat args [(str "-A:" (str/join ":" (cons ":org.babashka/defaults" aliases)))])
-                 cp (with-out-str (binding [deps/*env* env
-                                            deps/*extra-env* extra-env]
+                 bindings (cond->
+                            {#'deps/*env* env
+                             #'deps/*extra-env* extra-env}
+                            (:deps-root @bb-edn) (assoc #'deps/*dir* (:deps-root @bb-edn)))
+                 cp (with-out-str (with-bindings bindings
                                     (apply deps/-main args)))
                  cp (str/trim cp)
                  cp (str/replace cp (re-pattern (str cp/path-sep "+$")) "")]
