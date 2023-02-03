c9c737446f5c52bc58de547c949608cef857ed7a
Michiel Borkent <michielborkent@gmail.com>
2023-02-03 12:07:01 +0100

    wip [skip ci]

--- impl-java/src-java/babashka/impl/URLClassLoader.java
+++ impl-java/src-java/babashka/impl/URLClassLoader.java
@@ -1,6 +1,12 @@
 package babashka.impl;
 
-public class URLClassLoader extends java.net.URLClassLoader {
+import java.util.WeakHashMap;
+import java.io.*;
+import java.util.Objects;
+import java.net.*;
+import java.util.jar.*;
+
+public class URLClassLoader extends java.net.URLClassLoader implements Closeable {
 
     public URLClassLoader(java.net.URL[] urls) {
         super(urls);
@@ -21,19 +27,73 @@ public class URLClassLoader extends java.net.URLClassLoader {
         return findResource(name);
     }
 
-    public java.io.InputStream getResourceAsStream(String name) {
-        System.out.println("name: " + name);
-        var stream = super.getResourceAsStream(name);
-        System.out.println("stream: " + stream);
-        return super.getResourceAsStream(name);
+    private WeakHashMap<Closeable,Void>
+        closeables = new WeakHashMap<>();
+
+    public InputStream getResourceAsStream(String name) {
+        Objects.requireNonNull(name);
+        URL url = getResource(name);
+        try {
+            if (url == null) {
+                return null;
+            }
+            URLConnection urlc = url.openConnection();
+            InputStream is = urlc.getInputStream();
+            if (urlc instanceof JarURLConnection) {
+                JarFile jar = ((JarURLConnection)urlc).getJarFile();
+                synchronized (closeables) {
+                    if (!closeables.containsKey(jar)) {
+                        closeables.put(jar, null);
+                    }
+                }
+            } else if (true) { // }(urlc instanceof sun.net.www.protocol.file.FileURLConnection) {
+                synchronized (closeables) {
+                    closeables.put(is, null);
+                }
+            }
+            return is;
+        } catch (IOException e) {
+            return null;
+        }
     }
+
 
     public java.util.Enumeration<java.net.URL> getResources(String name) throws java.io.IOException {
         return findResources(name);
     }
 
-    public void close() {
+    public void close() throws IOException {
         System.out.println("close");
+        super.close();
+
+        java.util.List<IOException> errors = new java.util.ArrayList<IOException>();
+
+        // now close any remaining streams.
+
+        synchronized (closeables) {
+            java.util.Set<Closeable> keys = closeables.keySet();
+            for (Closeable c : keys) {
+                try {
+                    c.close();
+                } catch (IOException ioex) {
+                    errors.add(ioex);
+                }
+            }
+            closeables.clear();
+        }
+
+        if (errors.isEmpty()) {
+            return;
+        }
+
+        IOException firstex = errors.remove(0);
+
+        // Suppress any remaining exceptions
+
+        for (IOException error: errors) {
+            firstex.addSuppressed(error);
+        }
+        throw firstex;
     }
 
 }
