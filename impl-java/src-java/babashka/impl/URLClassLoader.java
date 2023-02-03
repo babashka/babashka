ca1e2d7769aa126bbcb138da79eca1fbee641f00
Michiel Borkent <michielborkent@gmail.com>
2023-02-03 13:08:55 +0100

    Classloader interop improvements (#1480)

--- impl-java/src-java/babashka/impl/URLClassLoader.java
+++ impl-java/src-java/babashka/impl/URLClassLoader.java
@@ -1,6 +1,17 @@
+// This file is mostly a workaround for https://github.com/oracle/graal/issues/1956
+
 package babashka.impl;
 
-public class URLClassLoader extends java.net.URLClassLoader {
+import java.util.WeakHashMap;
+import java.io.*;
+import java.util.Objects;
+import java.net.*;
+import java.util.jar.*;
+
+public class URLClassLoader extends java.net.URLClassLoader implements Closeable {
+
+    private WeakHashMap<Closeable,Void>
+        closeables = new WeakHashMap<>();
 
     public URLClassLoader(java.net.URL[] urls) {
         super(urls);
@@ -13,4 +24,71 @@ public class URLClassLoader extends java.net.URLClassLoader {
     public void _addURL(java.net.URL url) {
         super.addURL(url);
     }
+
+    // calling super.getResource() returned nil in native-image
+    public java.net.URL getResource(String name) {
+        return findResource(name);
+    }
+
+    // calling super.getResourceAsStream() returned nil in native-image
+    public InputStream getResourceAsStream(String name) {
+        Objects.requireNonNull(name);
+        URL url = getResource(name);
+        try {
+            if (url == null) {
+                return null;
+            }
+            URLConnection urlc = url.openConnection();
+            InputStream is = urlc.getInputStream();
+            if (urlc instanceof JarURLConnection) {
+                JarFile jar = ((JarURLConnection)urlc).getJarFile();
+                synchronized (closeables) {
+                    if (!closeables.containsKey(jar)) {
+                        closeables.put(jar, null);
+                    }
+                }
+            } else {
+                synchronized (closeables) {
+                    closeables.put(is, null);
+                }
+            }
+            return is;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public java.util.Enumeration<java.net.URL> getResources(String name) throws java.io.IOException {
+        return findResources(name);
+    }
+
+    public void close() throws IOException {
+        super.close();
+
+        java.util.List<IOException> errors = new java.util.ArrayList<IOException>();
+
+        synchronized (closeables) {
+            java.util.Set<Closeable> keys = closeables.keySet();
+            for (Closeable c : keys) {
+                try {
+                    c.close();
+                } catch (IOException ex) {
+                    errors.add(ex);
+                }
+            }
+            closeables.clear();
+        }
+
+        if (errors.isEmpty()) {
+            return;
+        }
+
+        IOException firstEx = errors.remove(0);
+
+        for (IOException error: errors) {
+            firstEx.addSuppressed(error);
+        }
+        throw firstEx;
+    }
+
 }
--- project.clj
+++ project.clj
@@ -27,7 +27,7 @@
                  [nrepl/bencode "1.1.0"]
                  [borkdude/sci.impl.reflector "0.0.1"]
                  [org.babashka/sci.impl.types "0.0.2"]
-                 [org.babashka/babashka.impl.java "0.1.6"]
+                 [org.babashka/babashka.impl.java "0.1.8"]
                  [org.clojure/core.async "1.6.673"]
                  [org.clojure/test.check "1.1.1"]
                  [com.github.clj-easy/graal-build-time "0.1.0"]
