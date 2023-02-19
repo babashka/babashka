332c7c6ac8a8fac29848fe702e4c340954fe7aad
Michiel Borkent <michielborkent@gmail.com>
2023-02-19 13:09:18 +0100

    Babashka CLI v0.6.46

--- project.clj
+++ project.clj
@@ -34,7 +34,7 @@
                  [com.github.clj-easy/graal-build-time "0.1.0"]
                  [rewrite-clj/rewrite-clj "1.1.46"]
                  [insn/insn "0.5.2"]
-                 [org.babashka/cli "0.6.45"]
+                 [org.babashka/cli "0.6.46"]
                  [org.babashka/http-client "0.1.4"]]
   :plugins       [[org.kipz/lein-meta-bom "0.1.1"]]
   :metabom {:jar-name "metabom.jar"}
