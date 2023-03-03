4e52f0d34cb56f12fa4dbb12d92a91c34cc67490
Michiel Borkent <michielborkent@gmail.com>
2023-03-03 20:21:19 +0100

    Bump http-client

--- project.clj
+++ project.clj
@@ -36,7 +36,7 @@
                  [rewrite-clj/rewrite-clj "1.1.46"]
                  [insn/insn "0.5.2"]
                  [org.babashka/cli "0.6.46"]
-                 [org.babashka/http-client "0.1.5"]]
+                 [org.babashka/http-client "0.1.6"]]
   :plugins       [[org.kipz/lein-meta-bom "0.1.1"]]
   :metabom {:jar-name "metabom.jar"}
   :profiles {:feature/xml  {:source-paths ["feature-xml"]
