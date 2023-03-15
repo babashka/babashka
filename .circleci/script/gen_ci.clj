320ac4a183e164ed02533aa2d1900566afe9648c
Rahul De <rahul080327@gmail.com>
2023-03-15 14:16:51 +0000

    [#1521] remove branch filter from main config

--- .circleci/script/gen_ci.clj
+++ .circleci/script/gen_ci.clj
@@ -226,8 +226,7 @@ java -jar \"$jar\" --config .build/bb.edn --deps-root . release-artifact \"$refl
                                     "linux-aarch64-static"
                                     {:deploy {:filters  {:branches {:only "master"}}
                                               :requires ["jvm" "linux"]}}
-                                    {:docker {:filters  {:branches {:only "master"}}
-                                              :requires ["linux" "linux-static" "linux-aarch64"]}}]}))))
+                                    {:docker {:requires ["linux" "linux-static" "linux-aarch64"]}}]}))))
 
 (def skip-config
   {:skip-if-only [#".*.md$"
