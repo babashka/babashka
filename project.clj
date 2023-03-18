2c6485b87681a2999f0f63097892fe726ede946c
Michiel Borkent <michielborkent@gmail.com>
2023-03-18 11:13:19 +0100

    Bump edamame to 1.3.20

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                    :flaky :flaky}
   :jvm-opts ["--enable-preview"]
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [borkdude/edamame "1.1.17"]
+                 [borkdude/edamame "1.3.20"]
                  [borkdude/graal.locking "0.0.2"]
                  [org.clojure/tools.cli "1.0.214"]
                  [cheshire "5.11.0"]
