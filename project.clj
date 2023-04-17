9a6f48b98676f6b00e37337f02cde40480824f5b
Michiel Borkent <michielborkent@gmail.com>
2023-04-17 19:57:48 +0200

    Bump CLI

--- project.clj
+++ project.clj
@@ -35,7 +35,7 @@
                  [com.github.clj-easy/graal-build-time "0.1.0"]
                  [rewrite-clj/rewrite-clj "1.1.46"]
                  [insn/insn "0.5.2"]
-                 [org.babashka/cli "0.6.50"]
+                 [org.babashka/cli "0.7.51"]
                  [org.babashka/http-client "0.1.8"]]
   :plugins       [[org.kipz/lein-meta-bom "0.1.1"]]
   :metabom {:jar-name "metabom.jar"}
