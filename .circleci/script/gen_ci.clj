c13500cddb974ce758689ab778ddaf8047dd72e6
Rahuλ Dé <lispyclouds@users.noreply.github.com>
2023-05-01 13:15:03 +0100

    [#1524] fix docker orchestration (#1552)
    * [#1524] fix docker orchestration
    
    * [#1524] revert branch checks

--- .circleci/script/gen_ci.clj
+++ .circleci/script/gen_ci.clj
@@ -218,7 +218,7 @@ java -jar \"$jar\" --config .build/bb.edn --deps-root . release-artifact \"$refl
                                     {:deploy {:filters  {:branches {:only "master"}}
                                               :requires ["jvm" "linux"]}}
                                     {:docker {:filters  {:branches {:only "master"}}
-                                              :requires ["linux" "linux-static"]}}]}))))
+                                              :requires ["linux" "linux-static" "linux-aarch64-static"]}}]}))))
 
 (def skip-config
   {:skip-if-only [#".*.md$"
