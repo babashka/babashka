01684be2fe3a54df11d5a2440561fc159eb2a860
Michiel Borkent <michielborkent@gmail.com>
2024-06-29 23:16:44 +0200

    xcode

--- .circleci/script/gen_ci.clj
+++ .circleci/script/gen_ci.clj
@@ -192,7 +192,7 @@ java -jar \"$jar\" --config .build/bb.edn --deps-root . release-artifact \"$refl
   [shorted?]
   (let [docker-executor-conf  {:docker [{:image "circleci/clojure:openjdk-11-lein-2.9.8-bullseye"}]}
         machine-executor-conf {:machine {:image "ubuntu-2004:202111-01"}}
-        mac-executor-conf     {:macos {:xcode "12.5.1"}}
+        mac-executor-conf     {:macos {:xcode "13.4.1"}}
         linux-graalvm-home    (str "/home/circleci/graalvm-" graalvm-version)
         mac-graalvm-home      (format "/Users/distiller/graalvm-%s/Contents/Home" graalvm-version)]
     (ordered-map
