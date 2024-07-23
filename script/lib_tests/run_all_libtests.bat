d726a614da897c447321e887e8958d40116d40f0
lread <lee@dlread.com>
2024-07-23 19:15:04 -0400

    Support running of new babashka.process tests
    Default `BABASHKA_TEST_ENV` to `jvm` for libtests
    CI already understands this need, but defaulting in-script avoids
    having devs to understand/remember it too.
    
    Alter PATH as required for new babashka.process tests.
    
    In support of babashka/process#163, babashka/process#164

--- script/lib_tests/run_all_libtests.bat
+++ script/lib_tests/run_all_libtests.bat
@@ -1,6 +1,10 @@
+if not defined BABASHKA_TEST_ENV set BABASHKA_TEST_ENV=jvm
+
 if "%BABASHKA_TEST_ENV%" EQU "native" (set BB_CMD=.\bb) else (set BB_CMD=lein bb)
 
 set EDN=lib_tests.edn
+
+set PATH=%CD%\process\target\test\on-path;%PATH%
 
 .\bb -f script/lib_tests/bb_edn_from_deps.clj %EDN%
 
