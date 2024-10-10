6d0a84d626e05b6571e3b3684d87b87892b4aef8
Michiel Borkent <michielborkent@gmail.com>
2024-10-10 20:48:46 +0200

    yes

--- impl-java/build.clj
+++ impl-java/build.clj
@@ -26,7 +26,12 @@
                 :lib lib
                 :version version
                 :basis basis
-                :src-dirs ["src"]})
+                :src-dirs ["src"]
+                :pom-data
+                [[:licenses
+                  [:license
+                   [:name "MIT License"]
+                   [:url "https://opensource.org/license/mit/"]]]]})
   (b/copy-dir {:src-dirs ["src"]
                :target-dir class-dir})
   (b/jar {:class-dir class-dir
