23e9a0cdc296a66e81b989d2b902b27dc5f61bd0
Michiel Borkent <michielborkent@gmail.com>
2024-10-30 14:17:48 +0100

    Upgrade to timbre v6.6.0

--- project.clj
+++ project.clj
@@ -73,7 +73,7 @@
              :feature/selmer {:source-paths ["feature-selmer"]
                               :dependencies [[selmer/selmer "1.12.59"]]}
              :feature/logging {:source-paths ["feature-logging"]
-                               :dependencies [[com.taoensso/timbre "6.5.0"]
+                               :dependencies [[com.taoensso/timbre "6.6.0"]
                                               [org.clojure/tools.logging "1.1.0"]]}
              :feature/priority-map {:source-paths ["feature-priority-map"]
                                     :dependencies [[org.clojure/data.priority-map "1.1.0"]]}
