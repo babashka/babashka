c170cadddd98e180b4b56ac6d66598fea03d171b
Michiel Borkent <michielborkent@gmail.com>
2025-06-11 23:15:49 +0200

    markdown

--- src/babashka/impl/markdown.clj
+++ src/babashka/impl/markdown.clj
@@ -1,12 +1,7 @@
 (ns babashka.impl.markdown
   (:require [nextjournal.markdown]
-            [nextjournal.markdown.impl :as impl]
-            [nextjournal.markdown.transform]
             [nextjournal.markdown.utils]
-            [nextjournal.markdown.utils.emoji]
             [sci.core :as sci]))
-
-@@#'impl/visitChildren-meth
 
 (def markdown-namespace (sci/copy-ns nextjournal.markdown
                                      (sci/create-ns 'nextjournal.markdown nil)))
