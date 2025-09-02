9a51dde16c2e1b20bf660e90f23988ca0d300b2a
Michiel Borkent <michielborkent@gmail.com>
2025-09-02 21:33:10 +0200

    workaround for static, shouldn't have to though...

--- test/babashka/agent_test.clj
+++ test/babashka/agent_test.clj
@@ -6,5 +6,5 @@
 
 (deftest agent-binding-conveyance-test
   (let [prog
-        "(def ^:dynamic *foo* 1) (def a (agent nil)) (binding [*foo* 2] (send-off a (fn [_] *foo*))) (await a) @a"]
+        "(def ^:dynamic *foo* 1) (def a (agent nil)) (binding [*foo* 2] (send-off a (fn [_] *foo*))) (await a) (shutdown-agents) @a"]
     (is (= 2 (edn/read-string (test-utils/bb nil prog))))))
