75ecf1dd682e071866e578c7fa21ec53cf06842a
Michiel Borkent <michielborkent@gmail.com>
2026-02-17 13:57:43 +0100

    Bump graal-build-time to 1.0.5 for GraalVM 25 compatibility
    The old 0.1.0 version doesn't properly initialize Clojure classes at
    build time on GraalVM 25. The 1.0.5 version requires the explicit
    InitClojureClasses feature flag in native-image.properties.

--- project.clj
+++ project.clj
@@ -32,7 +32,7 @@
                  [org.babashka/babashka.impl.java "0.1.16"]
                  [org.clojure/core.async "1.8.741"]
                  [org.clojure/test.check "1.1.1"]
-                 [com.github.clj-easy/graal-build-time "0.1.0"]
+                 [com.github.clj-easy/graal-build-time "1.0.5"]
                  [org.babashka/impl-graal-features "0.0.1"]
                  [rewrite-clj/rewrite-clj "1.2.50"]
                  [insn/insn "0.5.2"]
