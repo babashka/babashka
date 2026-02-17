29647721a6b1c97075613bc6be49666a7c3d7d75
Michiel Borkent <michielborkent@gmail.com>
2026-02-17 11:14:14 +0100

    Bump babashka.impl.java to 0.1.16

--- project.clj
+++ project.clj
@@ -29,7 +29,7 @@
                  [nrepl/bencode "1.2.0"]
                  [borkdude/sci.impl.reflector "0.0.5"]
                  [org.babashka/sci.impl.types "0.0.3"]
-                 [org.babashka/babashka.impl.java "0.1.15"]
+                 [org.babashka/babashka.impl.java "0.1.16"]
                  [org.clojure/core.async "1.8.741"]
                  [org.clojure/test.check "1.1.1"]
                  [com.github.clj-easy/graal-build-time "0.1.0"]
