db24c9e56f4215e1f9dce149f5d9bee3a4d9e13e
Michiel Borkent <michielborkent@gmail.com>
2026-07-22 15:05:22 +0200

    Bound glob test to test-resources (symlinked dev checkouts hang file-seq)

--- test-resources/babashka/glob.clj
+++ test-resources/babashka/glob.clj
@@ -1,5 +1,8 @@
 (require '[clojure.java.io :as io])
 
+;; file-seq is rooted at test-resources on purpose: walking "." follows any
+;; symlink in a dev checkout (e.g. a node_modules link escaping the repo) and
+;; can hang forever
 (defn glob [pattern]
   (let [matcher (.getPathMatcher
                  (java.nio.file.FileSystems/getDefault)
@@ -9,6 +12,6 @@
            (filter #(.matches matcher (.normalize (.toPath %))))
            (map #(.relativize (.toURI (io/file ".")) (.toURI %)))
            (map #(.getPath %)))
-     (file-seq (io/file ".")))))
+     (file-seq (io/file "test-resources")))))
 
-(glob "*/doc/*.md") ;;=> ["sci/doc/libsci.md" "babashka.nrepl/doc/intro.md"]
+(glob "test-resources/babashka/*.clj") ;;=> ["test-resources/babashka/glob.clj" ...]
