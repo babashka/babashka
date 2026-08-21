2574909a27d94a65414b3680c9f204784fca2541
Michiel Borkent <michielborkent@gmail.com>
2026-08-21 15:09:20 +0200

    Remove local build helper

--- build-and-test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Spike helper: uberjar + native-image + ffi test suite, logged.
-set -eo pipefail
-cd "$(dirname "$0")"
-log="$1"
-{ script/uberjar && script/compile; } > "$log" 2>&1
-lein do clean, test babashka.ffi-test >> "$log" 2>&1
-tail -3 "$log"
