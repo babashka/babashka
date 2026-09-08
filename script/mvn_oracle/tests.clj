64e6ddcd43dd23c19ce7142a7f29bc811bd67526
Michiel Borkent <michielborkent@gmail.com>
2026-09-08 12:52:37 +0200

    mvn tests runner: build the command without nils, --image passed empty arguments

--- script/mvn_oracle/tests.clj
+++ script/mvn_oracle/tests.clj
@@ -16,9 +16,12 @@
 (def results
   (doall
    (for [t tests]
-     (let [{:keys [exit out]} (p/shell {:out :string :err :out :continue true
-                                        :extra-env {"CLOJURE_CLI_ALLOW_HTTP_REPO" "true"}}
-                                       "./bb" (when-not image? "-cp") (when-not image? "resources/src/babashka") t)
+     (let [cmd (cond-> ["./bb"]
+                 (not image?) (conj "-cp" "resources/src/babashka")
+                 true (conj t))
+           {:keys [exit out]} (apply p/shell {:out :string :err :out :continue true
+                                              :extra-env {"CLOJURE_CLI_ALLOW_HTTP_REPO" "true"}}
+                                     cmd)
            summary (or (last (re-seq #"Ran \d+ tests containing \d+ assertions\.\n\d+ failures, \d+ errors\." out))
                        "no summary")]
        (println (format "%-40s %s" (fs/file-name t) (str/replace summary "\n" ", ")))
