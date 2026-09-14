9fad8e7101767aaaffd6633c251ac4486540fce8
Michiel Borkent <michielborkent@gmail.com>
2026-09-14 12:51:10 +0200

    Bump babashka.cli to 0.12.91, pin Clerk lib tests to merged #822

--- project.clj
+++ project.clj
@@ -35,7 +35,7 @@
                  [org.babashka/impl-graal-features "0.0.1"]
                  [rewrite-clj/rewrite-clj "1.2.57"]
                  [insn/insn "0.5.4"]
-                 [org.babashka/cli "0.12.90"]
+                 [org.babashka/cli "0.12.91"]
                  [org.babashka/http-client "0.4.25"]
                  [org.jsoup/jsoup "1.23.2"]
                  [io.github.nextjournal/markdown "0.7.225"]
