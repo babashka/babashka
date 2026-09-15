5999be208169d2757d5e9dde14bbf6b24c47d529
Michiel Borkent <michielborkent@gmail.com>
2026-09-15 15:54:07 +0200

    Name temp files with SecureRandom, since ThreadLocalRandom repeats in bb processes started together

--- resources/src/babashka/babashka/impl/mvn/http.clj
+++ resources/src/babashka/babashka/impl/mvn/http.clj
@@ -194,10 +194,14 @@
             (throw (ex-info message {:url url :expected expected :actual actual}))
             (printerrln message)))))))
 
+(def ^:private random
+  ;; ThreadLocalRandom repeats its sequence in bb processes started together
+  (delay (java.security.SecureRandom.)))
+
 (defn temp-file
   "Returns a path next to file named file.<random>.tmp."
   [file]
-  (str file "." (Long/toUnsignedString (.nextLong (java.util.concurrent.ThreadLocalRandom/current))) ".tmp"))
+  (str file "." (Long/toUnsignedString (.nextLong ^java.security.SecureRandom @random)) ".tmp"))
 
 (defn move-into-place!
   "Moves tmp over file atomically, or copies it on Windows. Deletes tmp."
